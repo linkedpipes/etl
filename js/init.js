9c10a6b2200c068228ccabc77ada76fc53261d97
Jakub Klímek <klimek@ksi.mff.cuni.cz>
2016-01-29 16:29:28 +0100

    Ïnitial commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+	// Initialize collapse button
+	//$(".button-collapse").sideNav();
+	// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+	//$('.collapsible').collapsible();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
