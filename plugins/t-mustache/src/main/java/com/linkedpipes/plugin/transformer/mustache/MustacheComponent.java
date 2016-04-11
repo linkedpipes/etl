db6b3f02ed2960e3148fa2139722bfccdfca77d2
skodape <skodapetr@gmail.com>
2016-04-11 15:43:08 +0200

    Add t-Mustache DPU

---
+++ plugins/t-mustache/src/main/java/com/linkedpipes/plugin/transformer/mustache/MustacheComponent.java
@@ -0,0 +1,268 @@
+package com.linkedpipes.plugin.transformer.mustache;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import com.linkedpipes.etl.dataunit.sesame.api.rdf.SingleGraphDataUnit;
+import com.linkedpipes.etl.dataunit.system.api.files.WritableFilesDataUnit;
+import com.linkedpipes.etl.dpu.api.service.ProgressReport;
+import com.linkedpipes.etl.executor.api.v1.exception.NonRecoverableException;
+import com.linkedpipes.etl.dpu.api.executable.SimpleExecution;
+import com.linkedpipes.etl.dpu.api.Component;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.openrdf.model.IRI;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
+import org.openrdf.model.Value;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryResult;
+
+/**
+ *
+ * @author Petr Škoda
+ */
+public final class MustacheComponent implements SimpleExecution {
+
+    /**
+     * Used to hold metadata about loaded objects.
+     */
+    private static class ObjectMetadata {
+
+        /**
+         * If true object is used to generate output.
+         */
+        boolean output;
+
+        /**
+         * Used for object ordering.
+         */
+        Integer order = null;
+
+        /**
+         * If {@link #output} is true then specify name of the output file.
+         * If null some name is generated and used.
+         */
+        String fileName;
+
+        /**
+         * If {@link #output} is true then contains data.
+         */
+        Object data;
+
+    }
+
+    @Component.InputPort(id = "InputRdf")
+    public SingleGraphDataUnit input;
+
+    @Component.OutputPort(id = "OutputFiles")
+    public WritableFilesDataUnit output;
+
+    @Component.Configuration
+    public MustacheConfiguration configuration;
+
+    @Component.Inject
+    public ProgressReport progressReport;
+
+    @Override
+    public void execute(Context context) throws NonRecoverableException {
+        final MustacheFactory mustacheFactory = new DefaultMustacheFactory();
+        final Mustache mustache = mustacheFactory.compile(
+                new StringReader(configuration.getTemplate()), "template");
+        final Collection<ObjectMetadata> data = loadData();
+        progressReport.start(data.size());
+        Integer counter = 0;
+        for (ObjectMetadata object : loadData()) {
+            final String fileName;
+            counter += 1;
+            if (object.fileName != null) {
+                fileName = object.fileName;
+            } else {
+                fileName = "output_" + counter;
+            }
+            final File outputFile = output.createFile(fileName).toFile();
+            try (OutputStreamWriter outputStream = new OutputStreamWriter(
+                    new FileOutputStream(outputFile), "UTF8")) {
+                mustache.execute(outputStream, object.data).flush();
+            } catch (IOException ex) {
+                throw new ExecutionFailed("Can't write output file.", ex);
+            }
+            progressReport.entryProcessed();
+        }
+        progressReport.done();
+    }
+
+    /**
+     * Load RDF data.
+     *
+     * @return
+     * @throws NonRecoverableException
+     */
+    private Collection<ObjectMetadata> loadData()
+            throws NonRecoverableException {
+        final Map<Resource, ObjectMetadata> objectsInfo = new HashMap<>();
+        final Map<Resource, Map<IRI, List<Value>>> objects = new HashMap<>();
+        // Load basic informations about objects.
+        try (RepositoryConnection connection
+                = input.getRepository().getConnection()) {
+            try (RepositoryResult<Statement> result = connection.getStatements(
+                    null, null, null, input.getGraph())) {
+                while (result.hasNext()) {
+                    final Statement st = result.next();
+                    // Check for interest properties.
+                    final Resource resource = st.getSubject();
+                    switch (st.getPredicate().stringValue()) {
+                        case "http://www.w3.org/1999/02/22-rdf-syntax-ns#type":
+                            if (st.getObject().stringValue().equals(
+                                    configuration.getResourceClass())) {
+                                getObject(resource, objectsInfo).output = true;
+                            }
+                            break;
+                        case MustacheVocabulary.HAS_ORDER:
+                            getObject(resource, objectsInfo).order
+                                    = ((Literal) st.getObject()).intValue();
+                            break;
+                        case MustacheVocabulary.HAS_FILE_NAME:
+                            getObject(resource, objectsInfo).fileName
+                                    = st.getObject().stringValue();
+                            break;
+                        default:
+                            addStatement(resource, st.getPredicate(),
+                                    st.getObject(), objects);
+                            break;
+                    }
+                }
+            }
+        }
+        // Create output.
+        final List<ObjectMetadata> objectsOutput = new LinkedList<>();
+        for (Resource resource : objectsInfo.keySet()) {
+            final ObjectMetadata object = objectsInfo.get(resource);
+            if (!object.output) {
+                continue;
+            }
+            object.data = createDataObject(resource, objectsInfo, objects);
+            objectsOutput.add(object);
+        }
+        return objectsOutput;
+    }
+
+    private static Object createDataObject(Resource resource,
+            Map<Resource, ObjectMetadata> objectsInfo,
+            Map<Resource, Map<IRI, List<Value>>> objects) {
+        final Map<IRI, List<Value>> data = objects.get(resource);
+        if (data == null || data.isEmpty()) {
+            // There are no data, return empty object.
+            return Collections.EMPTY_MAP;
+        }
+        final Map<String, Object> result = new HashMap<>();
+        for (Map.Entry<IRI, List<Value>> entry : data.entrySet()) {
+            // Check type.
+            if (entry.getValue().isEmpty()) {
+                continue;
+            }
+            if (entry.getValue().get(0) instanceof Resource) {
+                // Contains references.
+                if (entry.getValue().size() == 1) {
+                    // A simple reference.
+                    result.put(entry.getKey().stringValue(),
+                            createDataObject((Resource) entry.getValue().get(0),
+                                    objectsInfo, objects));
+                } else {
+                    // Sort the list with references.
+                    final List<Value> values = new LinkedList<>();
+                    values.addAll(entry.getValue());
+                    values.sort((Value left, Value right) -> {
+                        final ObjectMetadata leftMeta
+                                = objectsInfo.get((Resource) left);
+                        final ObjectMetadata rightMeta
+                                = objectsInfo.get((Resource) right);
+                        if (leftMeta == null || rightMeta == null
+                                || leftMeta.order == null
+                                || rightMeta.order == null) {
+                            return 0;
+                        } else if (leftMeta.order < rightMeta.order) {
+                            return -1;
+                        } else if (leftMeta.order > rightMeta.order) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    });
+                    // Load new objects.
+                    final List<Object> newData
+                            = new ArrayList<>(entry.getValue().size());
+                    for (Value value : values) {
+                        newData.add(createDataObject((Resource) value,
+                                objectsInfo, objects));
+                    }
+                    result.put(entry.getKey().stringValue(), newData);
+                }
+            } else // Values.
+             if (entry.getValue().size() == 1) {
+                    result.put(entry.getKey().stringValue(),
+                            getValue(entry.getValue().get(0)));
+                } else {
+                    final List<Object> newData
+                            = new ArrayList<>(entry.getValue().size());
+                    for (Value value : entry.getValue()) {
+                        newData.add(getValue(value));
+                    }
+                    result.put(entry.getKey().stringValue(), newData);
+                }
+        }
+        return result;
+    }
+
+    private static Object getValue(Value value) {
+        if (value instanceof Literal) {
+            final Literal literal = (Literal) value;
+            switch (literal.getDatatype().stringValue()) {
+                case "http://www.w3.org/2001/XMLSchema#boolean":
+                    return literal.booleanValue();
+            }
+        }
+        return value.stringValue();
+    }
+
+    private static ObjectMetadata getObject(Resource resource,
+            Map<Resource, ObjectMetadata> data) {
+        if (!data.containsKey(resource)) {
+            data.put(resource, new ObjectMetadata());
+        }
+        return data.get(resource);
+    }
+
+    /**
+     * Add statement to the data object.
+     *
+     * @param subject
+     * @param predicate
+     * @param object
+     * @param data
+     */
+    private static void addStatement(Resource subject, IRI predicate,
+            Value object, Map<Resource, Map<IRI, List<Value>>> data) {
+        if (!data.containsKey(subject)) {
+            data.put(subject, new HashMap<>());
+        }
+        final Map<IRI, List<Value>> resource = data.get(subject);
+        if (!resource.containsKey(predicate)) {
+            resource.put(predicate, new LinkedList<>());
+        }
+        final List<Value> values = resource.get(predicate);
+        values.add(object);
+    }
+
+}
---
+++ plugins/t-mustache/src/main/java/com/linkedpipes/plugin/transformer/mustache/MustacheVocabulary.java
@@ -0,0 +1,22 @@
+package com.linkedpipes.plugin.transformer.mustache;
+
+/**
+ *
+ * @author Petr Škoda
+ */
+class MustacheVocabulary {
+
+    private static final String PREFIX
+            = "http://plugins.linkedpipes.com/ontology/t-mustache#";
+
+    public static final String CONFIG_CLASS = PREFIX + "Configuration";
+
+    public static final String HAS_CLASS = PREFIX + "class";
+
+    public static final String HAS_TEMPLATE = PREFIX + "template";
+
+    public static final String HAS_ORDER = PREFIX + "order";
+
+    public static final String HAS_FILE_NAME = PREFIX + "fileName";
+
+}
