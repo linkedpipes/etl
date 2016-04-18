c486092069827755f6a9329ef841d5280b18062f
skodape <skodapetr@gmail.com>
2016-04-18 12:08:31 +0200

    Add runtime configuration to t-sparqlConstruct

--- plugins/t-sparqlConstruct/src/main/java/com/linkedpipes/plugin/transformer/sparql/construct/SparqlConstruct.java
+++ plugins/t-sparqlConstruct/src/main/java/com/linkedpipes/plugin/transformer/sparql/construct/SparqlConstruct.java
@@ -5,11 +5,11 @@ import com.linkedpipes.etl.dataunit.sesame.api.rdf.WritableSingleGraphDataUnit;
 import com.linkedpipes.etl.executor.api.v1.exception.NonRecoverableException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.Update;
-import org.openrdf.query.impl.DatasetImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.linkedpipes.etl.dpu.api.executable.SimpleExecution;
 import com.linkedpipes.etl.dpu.api.Component;
+import org.openrdf.query.impl.SimpleDataset;
 
 /**
  *
@@ -21,6 +21,10 @@ public final class SparqlConstruct implements SimpleExecution {
 
     @Component.InputPort(id = "InputRdf")
     public SingleGraphDataUnit inputRdf;
+
+    @Component.ContainsConfiguration
+    @Component.InputPort(id = "Configuration")
+    public SingleGraphDataUnit configurationRdf;
 
     @Component.InputPort(id = "OutputRdf")
     public WritableSingleGraphDataUnit outputRdf;
@@ -37,7 +41,7 @@ public final class SparqlConstruct implements SimpleExecution {
         // Execute query - TODO We should check that they share the same repository!
         inputRdf.execute((connection) -> {
             final Update update = connection.prepareUpdate(QueryLanguage.SPARQL, query);
-            final DatasetImpl dataset = new DatasetImpl();
+            final SimpleDataset dataset = new SimpleDataset();
             dataset.addDefaultGraph(inputRdf.getGraph());
             dataset.setDefaultInsertGraph(outputRdf.getGraph());
             update.setDataset(dataset);
