b3e8411cf94b5034fc372fbd7965dbdd5e9f3f4a
Petr Škoda <skodapetr@gmail.com>
2016-05-16 12:36:33 +0200

    Update code format

--- executor-monitor/src/main/java/com/linkedpipes/etl/executor/monitor/execution/ExecutionChecker.java
+++ executor-monitor/src/main/java/com/linkedpipes/etl/executor/monitor/execution/ExecutionChecker.java
@@ -265,15 +265,14 @@ class ExecutionChecker {
                 // We have start and end event.
                 execution.setStatus(Execution.StatusType.FINISHED);
             }
-        } else // We are updating the status.
-         if (endEvent == null) {
-                // Here we can only change from queued to running.
-                if (execution.getStatus() == Execution.StatusType.QUEUED) {
-                    execution.setStatus(Execution.StatusType.RUNNING);
-                }
-            } else {
-                execution.setStatus(Execution.StatusType.FINISHED);
+        } else if (endEvent == null) {
+            // Here we can only change from queued to running.
+            if (execution.getStatus() == Execution.StatusType.QUEUED) {
+                execution.setStatus(Execution.StatusType.RUNNING);
             }
+        } else {
+            execution.setStatus(Execution.StatusType.FINISHED);
+        }
 
         if (execution.getStatus() == Execution.StatusType.RUNNING) {
             execution.setExecutionStatementsFull(executionStatements);
