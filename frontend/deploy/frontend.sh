6507e9e6b7fd2d056e13b16dab348597c9dc18d6
Petr Škoda <skodapetr@gmail.com>
2016-06-06 11:43:01 +0200

    Add library check to frontend deploy scripts
    Libraries were downloaded only on first execution, if changed the
    libraries were not updated causing error,  resolves #165.

--- frontend/deploy/frontend.sh
+++ frontend/deploy/frontend.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-if [ ! -d "./frontend/node_modules" ]; then
-    echo Downloading libraries ...
-    cd ./frontend
-    npm install
-    cd ..
-    echo Downloading libraries ... done
-fi
-echo Libraries are presented
+echo Checking libraries ...
+cd ./frontend
+npm install
+cd ..
+echo Checking libraries ... done
 echo Starting Nodejs ...
 cd frontend
 export configFileLocation=../configuration.properties
