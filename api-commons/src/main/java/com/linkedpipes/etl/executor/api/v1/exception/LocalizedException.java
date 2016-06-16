a41e70312612ecbb598bb014ec7e160d2379b82a
Petr Škoda <skodapetr@gmail.com>
2016-06-16 15:22:58 +0200

    Override getMessage method for LocalizedException

--- api-commons/src/main/java/com/linkedpipes/etl/executor/api/v1/exception/LocalizedException.java
+++ api-commons/src/main/java/com/linkedpipes/etl/executor/api/v1/exception/LocalizedException.java
@@ -1,28 +1,32 @@
 package com.linkedpipes.etl.executor.api.v1.exception;
 
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.helpers.MessageFormatter;
 
 /**
- * Exception with support for localization.
+ * Exception with support for localisation.
  *
- * The reference of arguments in message should by done by '{}' string. The cause exception should be given
- * as the last argument, if the cause exception is available.
+ * The reference of arguments in message should by done by '{}' string.
+ * The cause exception should be given as the last argument, if the cause
+ * exception is available.
  *
  * @author Petr Škoda
  */
 public class LocalizedException extends Exception {
-
-    private static final Logger LOG = LoggerFactory.getLogger(LocalizedException.class);
 
     /**
      * Used to store a string in a given language.
      */
     public static final class LocalizedString {
 
+        /**
+         * Language tag.
+         */
         private final String language;
 
+        /**
+         * Message, support '{}' as value place holders.
+         */
         private final String value;
 
         public LocalizedString(String value, String language) {
@@ -41,25 +45,37 @@ public class LocalizedException extends Exception {
     }
 
     /**
-     * For each language a message with placeholders for arguments.
+     * For each language a message with place holders for arguments.
      */
     protected final List<LocalizedString> messages;
 
+    /**
+     * Arguments referenced from the message.
+     */
     protected final Object[] args;
 
     public LocalizedException(List<LocalizedString> messages, Object... args) {
-        // For now use first message.
+        // For now use first message as representation
+        // of this exception.
         super(messages.get(0).value);
         // Initialize exception.
         if (args.length > 0) {
             if (args[args.length - 1] instanceof Exception) {
-                this.initCause((Exception)args[args.length - 1]);
+                this.initCause((Exception) args[args.length - 1]);
             }
         }
         this.messages = messages;
         this.args = args;
-        //
-        LOG.error("LocalizedException: " + messages.get(0).value, args);
+    }
+
+    @Override
+    public String getMessage() {
+        // Use first given message if it exists.
+        if (messages.isEmpty()) {
+            return "No message provided!";
+        }
+        final LocalizedString message = messages.iterator().next();
+        return MessageFormatter.arrayFormat(message.value, args).getMessage();
     }
 
 }
