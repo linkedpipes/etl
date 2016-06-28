e817ad88a7ba5601f87202ac42783205aace1035
Petr Škoda <skodapetr@gmail.com>
2016-06-28 13:14:06 +0200

    Add t-fileHasher component
    Resolves #102

---
+++ plugins/t-fileHasher/src/main/java/com/linkedpipes/plugin/transformer/filehasher/FileHasher.java
@@ -0,0 +1,111 @@
+package com.linkedpipes.plugin.transformer.filehasher;
+
+import com.linkedpipes.etl.component.api.Component;
+import com.linkedpipes.etl.component.api.service.ExceptionFactory;
+import com.linkedpipes.etl.dataunit.sesame.api.rdf.WritableSingleGraphDataUnit;
+import com.linkedpipes.etl.dataunit.system.api.files.FilesDataUnit;
+import com.linkedpipes.etl.executor.api.v1.exception.LpException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.openrdf.model.IRI;
+import org.openrdf.model.Resource;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.SimpleValueFactory;
+import org.openrdf.model.vocabulary.RDF;
+
+/**
+ *
+ * @author Petr Škoda
+ */
+public class FileHasher implements Component.Sequential {
+
+    private static final int BUFFER_SIZE = 4096;
+
+    @Component.OutputPort(id = "InputFiles")
+    public FilesDataUnit inputFiles;
+
+    @Component.OutputPort(id = "OutputRdf")
+    public WritableSingleGraphDataUnit outputRdf;
+
+    @Component.Inject
+    public ExceptionFactory exceptionFactory;
+
+    @Override
+    public void execute() throws LpException {
+        final ValueFactory vf = SimpleValueFactory.getInstance();
+        final IRI typeHexBinary = vf.createIRI(
+                "http://www.w3.org/2001/XMLSchema#hexBinary");
+        for (FilesDataUnit.Entry entry : inputFiles) {
+            // Compute checksum.
+            final String checkSum;
+            try {
+                checkSum = computeChecksum(entry.toFile());
+            } catch (IOException ex) {
+                throw exceptionFactory.failed("Can't read file.", ex);
+            } catch (NoSuchAlgorithmException ex) {
+                throw exceptionFactory.failed("Missing algorithm.", ex);
+            }
+            // Create RDF output.
+            outputRdf.execute((connection) -> {
+                final Resource root = vf.createBNode();
+                final Resource checkSumNode = vf.createBNode();
+                connection.begin();
+                // Root object.
+                connection.add(vf.createStatement(root,
+                        vf.createIRI(FileHasherVocabulary.HAS_FILE_NAME),
+                        vf.createLiteral(entry.getFileName())
+                ), outputRdf.getGraph());
+                connection.add(vf.createStatement(root,
+                        vf.createIRI(FileHasherVocabulary.HAS_CHECKSUM),
+                        checkSumNode), outputRdf.getGraph());
+                // Checksum object.
+                connection.add(vf.createStatement(checkSumNode,
+                        RDF.TYPE,
+                        vf.createIRI(FileHasherVocabulary.HAS_CHECKSUM)
+                ), outputRdf.getGraph());
+                connection.add(vf.createStatement(checkSumNode,
+                        vf.createIRI(FileHasherVocabulary.HAS_ALGORITHM),
+                        vf.createIRI(FileHasherVocabulary.SHA1)
+                ), outputRdf.getGraph());
+                connection.add(vf.createStatement(checkSumNode,
+                        vf.createIRI(FileHasherVocabulary.HAS_CHECKSUM_VALUE),
+                        vf.createLiteral(checkSum, typeHexBinary)
+                ), outputRdf.getGraph());
+                connection.commit();
+            });
+        }
+    }
+
+    /**
+     * Compute SHA1 checksum for given file. Based on the code example
+     * from http://www.sha1-online.com/sha1-java/ .
+     *
+     * @param file
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     */
+    protected String computeChecksum(File file)
+            throws NoSuchAlgorithmException, IOException {
+        final MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+        try (FileInputStream stream = new FileInputStream(file)) {
+            final byte[] data = new byte[BUFFER_SIZE];
+            int read;
+            while ((read = stream.read(data)) != -1) {
+                sha1.update(data, 0, read);
+            }
+            // Convert to string.
+            final byte[] hashBytes = sha1.digest();
+            final StringBuffer hashString = new StringBuffer();
+            for (int i = 0; i < hashBytes.length; i++) {
+                hashString.append(Integer.toString(
+                        (hashBytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return hashString.toString();
+        }
+    }
+
+}
---
+++ plugins/t-fileHasher/src/main/java/com/linkedpipes/plugin/transformer/filehasher/FileHasherVocabulary.java
@@ -0,0 +1,30 @@
+package com.linkedpipes.plugin.transformer.filehasher;
+
+/**
+ *
+ * @author Petr Škoda
+ */
+final class FileHasherVocabulary {
+
+    private static final String PREFIX
+            = "http://plugins.linkedpipes.com/ontology/t-filehash#";
+
+    static final String HAS_FILE_NAME = PREFIX + "fileName";
+
+    private static final String SPDX
+            = "http://spdx.org/rdf/terms#";
+
+    static final String CHECKSUM = SPDX + "Checksum";
+
+    static final String HAS_CHECKSUM = SPDX + "checksum";
+
+    static final String HAS_ALGORITHM = SPDX + "algorithm";
+
+    static final String HAS_CHECKSUM_VALUE = SPDX + "checksumValue";
+
+    static final String SHA1 = SPDX + "checksumAlgorithm_sha1";
+
+    private FileHasherVocabulary() {
+    }
+
+}
