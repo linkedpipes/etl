d410feacef0109b98bfbf09759a67b973333c607
Petr Škoda <skodapetr@gmail.com>
2016-08-27 15:24:37 +0200

    Update pipeline filter

--- frontend/public/app/components/pipelines/listView/pipelineListCtrl.js
+++ frontend/public/app/components/pipelines/listView/pipelineListCtrl.js
@@ -1,6 +1,6 @@
 define([], function () {
     function controler($scope, $location, $http, $timeout, $mdDialog, $mdMedia,
-            refreshService, statusService, jsonldService, clipboard) {
+                       refreshService, statusService, jsonldService, clipboard) {
 
         var template = {
             'iri': {
@@ -12,14 +12,33 @@ define([], function () {
             'label': {
                 '$property': 'http://www.w3.org/2004/02/skos/core#prefLabel'
             },
-            'tags' : {
+            'tags': {
                 '$property': 'http://etl.linkedpipes.com/ontrology/tag',
-                '$type' : 'array'
+                '$type': 'array'
             }
         };
 
+        $scope.searchLabel = '';
+        $scope.searchTags = [];
+
+        $scope.search = {
+            'tags': {
+                'searchText': '',
+                // List of al tags, for filtering we use integers as tag
+                // indexes instead of strings. This value is also use
+                // to help use with filling in tags.
+                'all': []
+            },
+        };
+
+        $scope.search.tags.querySearch = function (query) {
+            return $scope.search.tags.all.filter(function (item) {
+                return item.indexOf(query) !== -1;
+            });
+        };
+
         $scope.info = {
-            'clipboard' : clipboard.supported
+            'clipboard': clipboard.supported
         };
 
         $scope.repository = jsonldService.createRepository({
@@ -37,17 +56,29 @@ define([], function () {
                 }
             },
             'decorator': function (item) {
-                if (!item['tags']) {
-                    item['tags'] = [];
-                    item['searchValue'] = '';
-                } else {
-                    var search = '';
-                    item['tags'].forEach(function (tag) {
-                        search += ' ' + tag.toLowerCase();
-                    });
-                    item['searchValue'] = search;
-                }
+                item['searchLabel'] = item['label'].toLowerCase();
+                // Show by default.
                 item['show'] = true;
+                item['filterLabel'] = true;
+                item['filterTags'] = true;
+                // The rest is only for pipelines with tags.
+                if (!item['tags'] || item['tags'].length == 0) {
+                    item['tags'] = [];
+                    return;
+                }
+                // Get indexes of our tags.
+                // var tagIndexes = [];
+                item['tags'].forEach(function (tag) {
+                    var tagIndex = $scope.search.tags.all.indexOf(tag);
+                    if (tagIndex === -1) {
+                        // tagIndex = $scope.search.tags.all.length;
+                        $scope.search.tags.all.push(tag);
+                    }
+                    // tagIndexes.push(tagIndex);
+                });
+                // We use integers instead of strings.
+                // tagIndexes.sort();
+                // item['searchTags'] = tagIndexes;
             },
             'url': '/resources/pipelines'
         });
@@ -60,14 +91,14 @@ define([], function () {
 
         $scope.onExecute = function (pipeline) {
             $http.post('/resources/executions?pipeline=' + pipeline.iri)
-                    .then(function (response) {
-                        $location.path('/executions').search({});
-                    }, function (response) {
-                        statusService.postFailed({
-                            'title': "Can't start the execution.",
-                            'response': response
-                        });
+                .then(function (response) {
+                    $location.path('/executions').search({});
+                }, function (response) {
+                    statusService.postFailed({
+                        'title': "Can't start the execution.",
+                        'response': response
                     });
+                });
         };
 
         $scope.onExport = function (pipeline, $event) {
@@ -116,7 +147,7 @@ define([], function () {
         $scope.onCopy = function (pipeline) {
             var id = 'created-' + (new Date()).getTime();
             var url = '/resources/pipelines/' + id + '?pipeline='
-                    + pipeline.iri;
+                + pipeline.iri;
             $http.post(url).then(function (response) {
                 statusService.success({
                     'title': 'Pipeline has been successfully copied.'
@@ -131,18 +162,18 @@ define([], function () {
             });
         };
 
-        $scope.onCopyIri = function(pipeline) {
+        $scope.onCopyIri = function (pipeline) {
             clipboard.copyText(pipeline.iri);
         };
 
         $scope.onDelete = function (pipeline, event) {
             var confirm = $mdDialog.confirm()
-                    .title('Would you like to delete pipeline "'
-                            + pipeline.label + '"?')
-                    .ariaLabel('Delete pipeline.')
-                    .targetEvent(event)
-                    .ok('Delete pipeline')
-                    .cancel('Cancel');
+                .title('Would you like to delete pipeline "'
+                    + pipeline.label + '"?')
+                .ariaLabel('Delete pipeline.')
+                .targetEvent(event)
+                .ok('Delete pipeline')
+                .cancel('Cancel');
             $mdDialog.show(confirm).then(function () {
                 $scope.repository.delete(pipeline);
                 // TODO Do not force reload here, use update.
@@ -150,33 +181,61 @@ define([], function () {
             });
         };
 
-        $scope.$watch("search", function (newValue, oldValue) {
-                if (newValue === oldValue) {
+        $scope.$watch('searchLabel', function (newValue, oldValue) {
+            if (newValue === oldValue) {
+                return;
+            }
+            // Special care for empty query.
+            if (newValue === '') {
+                $scope.repository.data.forEach(function (item) {
+                    item['filterLabel'] = true;
+                    item['show'] = item['filterLabel'] && item['filterTags'];
+                });
+                return;
+            }
+            //
+            var query = newValue.toLowerCase();
+            $scope.repository.data.forEach(function (item) {
+                item['filterLabel'] = item['searchLabel'].indexOf(query) !== -1;
+                item['show'] = item['filterLabel'] && item['filterTags'];
+            });
+        });
+
+        $scope.chipsFilter = function () {
+            // Special for no tags.
+            if ($scope.searchTags.length === 0) {
+                $scope.repository.data.forEach(function (item) {
+                    item['filterTags'] = true;
+                    item['show'] = item['filterLabel'] && item['filterTags'];
+                });
+                return;
+            }
+            $scope.repository.data.forEach(function (item) {
+                if (item.tags.length < $scope.searchTags.length) {
+                    item['filterTags'] = false;
+                    item['show'] = item['filterLabel'] && item['filterTags'];
                     return;
                 }
-                var filters = newValue.toLowerCase().split(' ');
-                $scope.repository.data.forEach(function (item) {
-                    for (var index in filters) {
-                        filter = filters[index];
-                        if (item['searchValue'].indexOf(filter) === -1) {
-                            item['show'] = false;
-                            return;
-                        }
+                item['filterTags'] = true;
+                for (var index in $scope.searchTags) {
+                    if (item.tags.indexOf($scope.searchTags[index]) === -1) {
+                        item['filterTags'] = false;
+                        break;
                     }
-                    item['show'] = true;
-                    // item['show'] = item['searchValue'].indexOf(filter) !== -1;
-                });
-            }
-        );
+                }
+                item['show'] = item['filterLabel'] && item['filterTags'];
+            });
+        };
 
         var initialize = function () {
-            $scope.repository.load(function () { },
-                    function (response) {
-                        statusService.getFailed({
-                            'title': "Can't load data.",
-                            'response': response
-                        });
+            $scope.repository.load(function () {
+                },
+                function (response) {
+                    statusService.getFailed({
+                        'title': "Can't load data.",
+                        'response': response
                     });
+                });
 
             refreshService.set(function () {
                 // TODO Enable update once the server has
@@ -186,6 +245,7 @@ define([], function () {
         };
         $timeout(initialize, 0);
     }
+
     //
     controler.$inject = ['$scope', '$location', '$http', '$timeout',
         '$mdDialog', '$mdMedia', 'service.refresh',
@@ -194,5 +254,6 @@ define([], function () {
     function init(app) {
         app.controller('components.pipelines.list', controler);
     }
+
     return init;
 });
