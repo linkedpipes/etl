f8df2ad0940603077db28c0d825591573c89a7cf
Petr Škoda <skodapetr@gmail.com>
2016-09-19 07:45:34 +0200

    Add template support for t-sparqlSelect

--- plugins/t-sparqlSelect/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/t-sparqlSelect/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -1,39 +1,58 @@
 define([], function () {
+
+    const PREFIX = 'http://plugins.linkedpipes.com/ontology/t-sparqlSelect#';
+
     function controller($scope, $service, rdfService) {
 
-        $scope.dialog = {
-            'query': '',
-            'fileName': ''
-        };
+        $scope.dialog = {};
 
-        var rdf = rdfService.create('http://plugins.linkedpipes.com/ontology/t-sparqlSelect#');
+        if ($scope.control === undefined) {
+            $scope.control = {};
+        }
+
+        var rdf = rdfService.create('');
 
         function loadDialog() {
             rdf.setData($service.config.instance);
-            var resource = rdf.secureByType('Configuration');
-
-            $scope.dialog.query = rdf.getString(resource, 'query');
-            $scope.dialog.fileName = rdf.getString(resource, 'fileName');
-        };
+            var resource = rdf.secureByType(PREFIX + 'Configuration');
+            //
+            $scope.dialog.query = rdf.getString(resource,
+                PREFIX + 'query');
+            $scope.dialog.fileName = rdf.getString(resource,
+                PREFIX + 'fileName');
+            //
+            $scope.control.query = $service.control.fromIri(
+                rdf.getIri(resource, PREFIX + 'queryControl'));
+            $scope.control.fileName = $service.control.fromIri(
+                rdf.getIri(resource, PREFIX + 'fileNameControl'));
+        }
 
         function saveDialog() {
-            var resource = rdf.secureByType('Configuration');
+            rdf.setData($service.config.instance);
+            var resource = rdf.secureByType(PREFIX + 'Configuration');
+            //
+            if (!$scope.control.query.forced) {
+                rdf.setString(resource, PREFIX + 'query',
+                    $scope.dialog.query);
+            }
+            if (!$scope.control.fileName.forced) {
+                rdf.setString(resource, PREFIX + 'fileName',
+                    $scope.dialog.fileName);
+            }
+            //
+            rdf.setIri(resource, PREFIX + 'queryControl',
+                $service.control.toIri($scope.control.query));
+            rdf.setIri(resource, PREFIX + 'fileNameControl',
+                $service.control.toIri($scope.control.fileName));
+        }
 
-            rdf.setString(resource, 'query', $scope.dialog.query);
-            rdf.setString(resource, 'fileName', $scope.dialog.fileName);
-
-            return rdf.getData();
-        };
-
-        // Define the save function.
         $service.onStore = function () {
             saveDialog();
         }
 
-        // Load data.
         loadDialog();
     }
-    //
+
     controller.$inject = ['$scope', '$service', 'services.rdf.0.0.0'];
     return controller;
 });
