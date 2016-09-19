7fee76fe3c7e1ee1ff10302ff331a7fc20ae56a4
Petr Škoda <skodapetr@gmail.com>
2016-09-19 07:43:10 +0200

    Add template support for t-filesFilter

--- plugins/t-filesFilter/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/t-filesFilter/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -1,36 +1,48 @@
 define([], function () {
+
+    const PREFIX = 'http://plugins.linkedpipes.com/ontology/t-filesFilter#';
+
     function controller($scope, $service, rdfService) {
 
-        $scope.dialog = {
-            'pattern': ''
-        };
+        $scope.dialog = {};
 
-        var rdf = rdfService.create('http://plugins.linkedpipes.com/ontology/t-filesFilter#');
+        if ($scope.control === undefined) {
+            $scope.control = {};
+        }
+
+        var rdf = rdfService.create('');
 
         function loadDialog() {
             rdf.setData($service.config.instance);
-            var resource = rdf.secureByType('Configuration');
-
-            $scope.dialog.pattern = rdf.getString(resource, 'fileNamePattern');
-        };
+            var resource = rdf.secureByType(PREFIX + 'Configuration');
+            //
+            $scope.dialog.pattern = rdf.getString(resource,
+                PREFIX + 'fileNamePattern');
+            //
+            $scope.control.fileNamePattern = $service.control.fromIri(
+                rdf.getIri(resource, PREFIX + 'fileNamePatternControl'));
+        }
 
         function saveDialog() {
-            var resource = rdf.secureByType('Configuration');
+            rdf.setData($service.config.instance);
+            var resource = rdf.secureByType(PREFIX + 'Configuration');
+            //
+            if (!$scope.control.fileNamePattern.forced) {
+                rdf.setString(resource, PREFIX + 'fileNamePattern',
+                    $scope.dialog.pattern);
+            }
+            //
+            rdf.setIri(resource, PREFIX + 'fileNamePatternControl',
+                $service.control.toIri($scope.control.fileNamePattern));
+        }
 
-            rdf.setString(resource, 'fileNamePattern', $scope.dialog.pattern);
-
-            return rdf.getData();
-        };
-
-        // Define the save function.
         $service.onStore = function () {
             saveDialog();
         }
 
-        // Load data.
         loadDialog();
     }
-    //
+
     controller.$inject = ['$scope', '$service', 'services.rdf.0.0.0'];
     return controller;
 });
