f257e2b4e1517c92baa8496dbdd49afef9680965
Petr Škoda <skodapetr@gmail.com>
2016-09-19 07:44:32 +0200

    Add template support for t-filesToRdf

--- plugins/t-filesToRdf/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/t-filesToRdf/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -1,42 +1,61 @@
 define([], function () {
+
+    const PREFIX = 'http://plugins.linkedpipes.com/ontology/t-filesToRdf#';
+
     function controller($scope, $service, rdfService) {
 
-        $scope.dialog = {
-            'commitSize': '',
-            'mimeType': ''
-        };
+        $scope.dialog = {};
 
-        var rdf = rdfService.create('http://plugins.linkedpipes.com/ontology/t-filesToRdf#');
+        if ($scope.control === undefined) {
+            $scope.control = {};
+        }
+
+        var rdf = rdfService.create('');
 
         function loadDialog() {
             rdf.setData($service.config.instance);
-            var resource = rdf.secureByType('Configuration');
-
-            $scope.dialog.commitSize = rdf.getInteger(resource, 'commitSize');
-            $scope.dialog.mimeType = rdf.getString(resource, 'mimeType');
+            var resource = rdf.secureByType(PREFIX + 'Configuration');
+            //
+            $scope.dialog.commitSize = rdf.getInteger(resource,
+                PREFIX + 'commitSize');
+            $scope.dialog.mimeType = rdf.getString(resource,
+                PREFIX + 'mimeType');
             if ($scope.dialog.mimeType === undefined) {
-                $scope.dialog.mimeType = "";
+                $scope.dialog.mimeType = '';
             }
-        };
+            //
+            $scope.control.commitSize = $service.control.fromIri(
+                rdf.getIri(resource, PREFIX + 'commitSizeControl'));
+            $scope.control.mimeType = $service.control.fromIri(
+                rdf.getIri(resource, PREFIX + 'mimeTypeControl'));
+        }
 
         function saveDialog() {
-            var resource = rdf.secureByType('Configuration');
+            rdf.setData($service.config.instance);
+            var resource = rdf.secureByType(PREFIX + 'Configuration');
+            //
+            if (!$scope.control.commitSize.forced) {
+                rdf.setInteger(resource, PREFIX + 'commitSize',
+                    $scope.dialog.commitSize);
+            }
+            if (!$scope.control.mimeType.forced) {
+                rdf.setString(resource, PREFIX + 'mimeType',
+                    $scope.dialog.mimeType);
+            }
+            //
+            rdf.setIri(resource, PREFIX + 'commitSizeControl',
+                $service.control.toIri($scope.control.commitSize));
+            rdf.setIri(resource, PREFIX + 'mimeTypeControl',
+                $service.control.toIri($scope.control.mimeType));
+        }
 
-            rdf.setInteger(resource, 'commitSize', $scope.dialog.commitSize);
-            rdf.setString(resource, 'mimeType', $scope.dialog.mimeType);
-
-            return rdf.getData();
-        };
-
-        // Define the save function.
         $service.onStore = function () {
             saveDialog();
         }
 
-        // Load data.
         loadDialog();
     }
-    //
+
     controller.$inject = ['$scope', '$service', 'services.rdf.0.0.0'];
     return controller;
 });
