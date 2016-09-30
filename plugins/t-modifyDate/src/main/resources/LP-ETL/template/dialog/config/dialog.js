8702e4217c0238bd75a61b35d44a39037c41b1cd
Petr Škoda <skodapetr@gmail.com>
2016-09-30 07:32:00 +0200

    Update t-modifyDate

--- plugins/t-modifyDate/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/t-modifyDate/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -1,65 +1,46 @@
 define([], function () {
+    "use strict";
 
-    const PREFIX = 'http://plugins.linkedpipes.com/ontology/t-modifyDate#';
+    const DESC = {
+        "$namespace" :
+            "http://plugins.linkedpipes.com/ontology/t-modifyDate#",
+        "$type": "Configuration",
+        "input" : {
+            "$type" : "iri",
+            "$property" : "input",
+            "$control": "inputControl",
+            "$label" : "Input predicate"
+        },
+        "shiftBy" : {
+            "$type" : "int",
+            "$property" : "shiftBy",
+            "$control": "shiftByControl",
+            "$label" : "Shift date by"
+        },
+        "output" : {
+            "$type" : "iri",
+            "$property" : "output",
+            "$control": "outputControl",
+            "$label" : "Output predicate"
+        }
+    };
 
-    function controller($scope, $service, rdfService) {
+    function controller($scope, $service) {
 
-        $scope.dialog = {};
-
-        if ($scope.control === undefined) {
-            $scope.control = {};
+        if ($scope.dialog === undefined) {
+            $scope.dialog = {};
         }
 
-        var rdf = rdfService.create('');
-
-        function loadDialog() {
-            rdf.setData($service.config.instance);
-            var resource = rdf.secureByType(PREFIX + 'Configuration');
-            //
-            $scope.dialog.input = rdf.getString(resource, PREFIX + 'input');
-            $scope.dialog.shiftBy = rdf.getInteger(resource, PREFIX + 'shiftBy');
-            $scope.dialog.output = rdf.getString(resource, PREFIX + 'output');
-            //
-            $scope.control.input = $service.control.fromIri(
-                rdf.getIri(resource, PREFIX + 'inputControl'));
-            $scope.control.shiftBy = $service.control.fromIri(
-                rdf.getIri(resource, PREFIX + 'shiftByControl'));
-            $scope.control.output = $service.control.fromIri(
-                rdf.getIri(resource, PREFIX + 'outputControl'));
-        }
-
-        function saveDialog() {
-            rdf.setData($service.config.instance);
-            var resource = rdf.secureByType(PREFIX + 'Configuration');
-            //
-            if (!$scope.control.input.forced) {
-                rdf.setString(resource, PREFIX + 'input',
-                    $scope.dialog.input);
-            }
-            if (!$scope.control.shiftBy.forced) {
-                rdf.setInteger(resource, PREFIX + 'shiftBy',
-                    $scope.dialog.shiftBy);
-            }
-            if (!$scope.control.output.forced) {
-                rdf.setString(resource, PREFIX + 'output',
-                    $scope.dialog.output);
-            }
-            //
-            rdf.setIri(resource, PREFIX + 'inputControl',
-                $service.control.toIri($scope.control.input));
-            rdf.setIri(resource, PREFIX + 'shiftByControl',
-                $service.control.toIri($scope.control.shiftBy));
-            rdf.setIri(resource, PREFIX + 'outputControl',
-                $service.control.toIri($scope.control.output));
-        }
+        const dialogManager = $service.v1.manager(DESC, $scope.dialog);
 
         $service.onStore = function () {
-            saveDialog();
-        }
+            dialogManager.save();
+        };
 
-        loadDialog();
+        dialogManager.load();
+
     }
 
-    controller.$inject = ['$scope', '$service', 'services.rdf.0.0.0'];
+    controller.$inject = ['$scope', '$service'];
     return controller;
 });
