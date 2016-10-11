b87a3f223e2fe9b2ed18b491e8683161a04d864b
Petr Škoda <skodapetr@gmail.com>
2016-10-11 13:36:48 +0200

    Update x-virtuso
    Update description and configuration dialog.

--- plugins/x-virtuoso/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/x-virtuoso/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -5,58 +5,44 @@ define([], function () {
         "$namespace" :
             "http://plugins.linkedpipes.com/ontology/x-virtuoso#",
         "$type": "Configuration",
+        "$options" : {
+            "$predicate": "auto",
+            "$control": "auto"
+        },
         "uri" : {
             "$type" : "str",
-            "$property" : "uri",
-            "$control": "uriControl",
             "$label" : "Virtuoso JDBC connection string"
         },
         "fileName" : {
             "$type" : "str",
-            "$property" : "fileName",
-            "$control": "fileNameControl",
             "$label" : "Filename to load"
         },
         "graph" : {
             "$type" : "iri",
-            "$property" : "graph",
-            "$control": "graphControl",
             "$label" : "Target graph IRI"
         },
         "directory" : {
             "$type" : "str",
-            "$property" : "directory",
-            "$control": "directoryControl",
             "$label" : "Remote directory with source files"
         },
         "clearGraph" : {
             "$type" : "bool",
-            "$property" : "clearGraph",
-            "$control": "clearGraphControl",
             "$label" : "Clear target graph"
         },
         "clearLoadList" : {
             "$type" : "bool",
-            "$property" : "clearSqlLoadTable",
-            "$control": "clearSqlLoadTableControl",
             "$label" : "Clear Virtuoso load list"
         },
         "username" : {
             "$type" : "str",
-            "$property" : "username",
-            "$control": "usernameControl",
             "$label" : "Virtuoso user name"
         },
         "password" : {
             "$type" : "str",
-            "$property" : "password",
-            "$control": "passwordControl",
             "$label" : "Virtuoso password"
         },
         "updateInterval" : {
             "$type" : "int",
-            "$property" : "updateInterval",
-            "$control": "updateIntervalControl",
             "$label" : "Status update interval"
         }
     };
