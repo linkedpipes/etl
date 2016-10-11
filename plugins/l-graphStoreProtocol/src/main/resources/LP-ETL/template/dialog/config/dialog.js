7ec1a164d277f4109ecc3ff6655cc2739e278b6e
Petr Škoda <skodapetr@gmail.com>
2016-10-11 08:25:54 +0200

    Update l-graphStoreProtocol
    Add inheritance support.

--- plugins/l-graphStoreProtocol/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/l-graphStoreProtocol/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -1,57 +1,66 @@
 define([], function () {
-    function controller($scope, $service, rdfService) {
+    "use strict";
 
-        $scope.dialog = {
-            'graph': '',
-            'repository': 'VIRTUOSO',
-            'authentification': false,
-            'user': '',
-            'password': '',
-            'checkSize': false,
-            'endpointSelect': '',
-            'endpoint': '',
-            'replace': false
-        };
+    const DESC = {
+        "$namespace": "http://plugins.linkedpipes.com/ontology/l-graphStoreProtocol#",
+        "$type": "Configuration",
+        "$control": {
+            "$predicate": "auto"
+        },
+        "graph": {
+            "$type": "str",
+            "$label": "Target graph IRI"
+        },
+        "repository": {
+            "$type": "int",
+            "$label": "Repository"
+        },
+        "authentification": {
+            "$type": "bool",
+            "$label": "Use authentication"
+        },
+        "user": {
+            "$type": "str",
+            "$label": "User name"
+        },
+        "password": {
+            "$type": "str",
+            "$label": "Password"
+        },
+        "checkSize": {
+            "$type": "bool",
+            "$label": "Log graph size"
+        },
+        "endpointSelect": {
+            "$type": "str",
+            "$label": "SPARQL Endpoint"
+        },
+        "endpoint": {
+            "$type": "str",
+            "$label": "Graph store protocol endpoint"
+        },
+        "replace": {
+            "$type": "bool",
+            "$label": "Clear target graph before loading"
+        }
+    };
 
-        var rdf = rdfService.create('http://plugins.linkedpipes.com/ontology/l-graphStoreProtocol#');
+    function controller($scope, $service) {
 
-        function loadDialog() {
-            rdf.setData($service.config.instance);
-            var resource = rdf.secureByType('Configuration');
-            $scope.dialog.graph = rdf.getString(resource, 'graph');
-            $scope.dialog.repository = rdf.getString(resource, 'repository');
-            $scope.dialog.authentification = rdf.getString(resource, 'authentification');
-            $scope.dialog.user = rdf.getString(resource, 'user');
-            $scope.dialog.password = rdf.getString(resource, 'password');
-            $scope.dialog.checkSize = rdf.getString(resource, 'checkSize');
-            $scope.dialog.endpointSelect = rdf.getString(resource, 'endpointSelect');
-            $scope.dialog.endpoint = rdf.getString(resource, 'endpoint');
-            $scope.dialog.replace = rdf.getBoolean(resource, 'replace');
-        };
-
-        function saveDialog() {
-            var resource = rdf.secureByType('Configuration');
-            rdf.setString(resource, 'graph', $scope.dialog.graph);
-            rdf.setString(resource, 'repository', $scope.dialog.repository);
-            rdf.setBoolean(resource, 'authentification', $scope.dialog.authentification);
-            rdf.setString(resource, 'user', $scope.dialog.user);
-            rdf.setString(resource, 'password', $scope.dialog.password);
-            rdf.setBoolean(resource, 'checkSize', $scope.dialog.checkSize);
-            rdf.setString(resource, 'endpointSelect', $scope.dialog.endpointSelect);
-            rdf.setString(resource, 'endpoint', $scope.dialog.endpoint);
-            rdf.setBoolean(resource, 'replace', $scope.dialog.replace);
-            return rdf.getData();
-        };
-
-        // Define the save function.
-        $service.onStore = function () {
-            saveDialog();
+        if ($scope.dialog === undefined) {
+            $scope.dialog = {};
         }
 
-        // Load data.
-        loadDialog();
+        const dialogManager = $service.v1.manager(DESC, $scope.dialog);
+
+        $service.onStore = function () {
+            dialogManager.save();
+        };
+
+        dialogManager.load();
+
     }
-    //
-    controller.$inject = ['$scope', '$service', 'services.rdf.0.0.0'];
+
+    controller.$inject = ['$scope', '$service'];
     return controller;
 });
