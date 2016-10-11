5c208b0cb781ff0b8793b9e7ec655cf079cae0fc
Petr Škoda <skodapetr@gmail.com>
2016-10-11 08:27:13 +0200

    Update l-sparqlEndpoint
    Add inheritance support.

--- plugins/l-sparqlEndpoint/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/l-sparqlEndpoint/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -1,51 +1,58 @@
 define([], function () {
-    function controller($scope, $service, rdfService) {
+    "use strict";
 
-        $scope.dialog = {
-            'endpoint': '',
-            'targetGraphURI': '',
-            'clearGraph': true,
-            'commitSize': '',
-            'useAuthentication': false,
-            'userName': '',
-            'password': ''
-        };
+    const DESC = {
+        "$namespace": "http://plugins.linkedpipes.com/ontology/l-graphStoreProtocol#",
+        "$type": "Configuration",
+        "$control": {
+            "$predicate": "auto"
+        },
+        "endpoint": {
+            "$type": "str",
+            "$label": "Endpoint"
+        },
+        "targetGraphURI": {
+            "$type": "int",
+            "$label": "Target graph IRI"
+        },
+        "clearGraph": {
+            "$type": "bool",
+            "$label": "Clear target graph before loading"
+        },
+        "commitSize": {
+            "$type": "int",
+            "$label": "Commit size"
+        },
+        "useAuthentication": {
+            "$type": "bool",
+            "$label": "Use authentication"
+        },
+        "userName": {
+            "$type": "str",
+            "$label": "User name"
+        },
+        "password": {
+            "$type": "str",
+            "$label": "Password"
+        }
+    };
 
-        var rdf = rdfService.create('http://plugins.linkedpipes.com/ontology/l-sparqlEndpoint#');
+    function controller($scope, $service) {
 
-        function loadDialog() {
-            rdf.setData($service.config.instance);
-            var resource = rdf.secureByType('Configuration');
-            $scope.dialog.endpoint = rdf.getString(resource, 'endpoint');
-            $scope.dialog.targetGraphURI = rdf.getString(resource, 'targetGraphURI');
-            $scope.dialog.clearGraph = rdf.getBoolean(resource, 'clearGraph');
-            $scope.dialog.commitSize = rdf.getInteger(resource, 'commitSize');
-            $scope.dialog.useAuthentication = rdf.getBoolean(resource, 'useAuthentification');
-            $scope.dialog.userName = rdf.getString(resource, 'userName');
-            $scope.dialog.password = rdf.getString(resource, 'password');
-        };
-
-        function saveDialog() {
-            var resource = rdf.secureByType('Configuration');
-            rdf.setString(resource, 'endpoint', $scope.dialog.endpoint);
-            rdf.setString(resource, 'targetGraphURI', $scope.dialog.targetGraphURI);
-            rdf.setBoolean(resource, 'clearGraph', $scope.dialog.clearGraph);
-            rdf.setInteger(resource, 'commitSize', $scope.dialog.commitSize);
-            rdf.setBoolean(resource, 'useAuthentification', $scope.dialog.useAuthentication);
-            rdf.setString(resource, 'userName', $scope.dialog.userName);
-            rdf.setString(resource, 'password', $scope.dialog.password);
-            return rdf.getData();
-        };
-
-        // Define the save function.
-        $service.onStore = function () {
-            saveDialog();
+        if ($scope.dialog === undefined) {
+            $scope.dialog = {};
         }
 
-        // Load data.
-        loadDialog();
+        const dialogManager = $service.v1.manager(DESC, $scope.dialog);
+
+        $service.onStore = function () {
+            dialogManager.save();
+        };
+
+        dialogManager.load();
+
     }
-    //
-    controller.$inject = ['$scope', '$service', 'services.rdf.0.0.0'];
+
+    controller.$inject = ['$scope', '$service'];
     return controller;
 });
