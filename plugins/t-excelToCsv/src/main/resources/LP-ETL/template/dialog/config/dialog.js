8d0397302dc60ceb1322bca0fc312ab4e49d2cb7
Petr Škoda <skodapetr@gmail.com>
2016-10-25 15:09:53 +0200

    Add control support to t-excelToCsv

--- plugins/t-excelToCsv/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/t-excelToCsv/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -1,68 +1,71 @@
 define([], function () {
-    function controller($scope, $service, rdfService) {
+    "use strict";
 
-        $scope.dialog = {
-            'fileName': '',
-            'sheetFilter': '',
-            'rowStart': '',
-            'rowEnd': '',
-            'columnStart': '',
-            'columnEnd': '',
-            'header': false,
-            'numericParse': false,
-            'skipEmptyRows': false,
-            'includeSheetName': false
-        };
+    const DESC = {
+        "$namespace": "http://plugins.linkedpipes.com/ontology/t-excelToCsv#",
+        "$type": "Configuration",
+        "$options": {
+            "$predicate": "auto",
+            "$control": "auto"
+        },
+        "fileName": {
+            "$type": "str",
+            "$label": "Output file name<"
+        },
+        "sheetFilter": {
+            "$type": "str",
+            "$label": "Sheet filter"
+        },
+        "rowStart": {
+            "$type": "int",
+            "$label": "Row start"
+        },
+        "rowEnd": {
+            "$type": "int",
+            "$label": "Row end"
+        },
+        "columnStart": {
+            "$type": "int",
+            "$label": "Column start"
+        },
+        "columnEnd": {
+            "$type": "int",
+            "$label": "Column end"
+        },
+        "header": {
+            "$type": "bool",
+            "$label": "Virtual columns with header"
+        },
+        "numericParse": {
+            "$type": "bool",
+            "$label": "Determine type for numeric cells"
+        },
+        "skipEmptyRows": {
+            "$type": "bool",
+            "$label": "Skip empty rows"
+        },
+        "includeSheetName": {
+            "$type": "bool",
+            "$label": "Add sheet name as a column"
+        }
+    };
 
-        var rdf = rdfService.create('http://plugins.linkedpipes.com/ontology/t-excelToCsv#');
+    function controller($scope, $service) {
 
-        function loadDialog() {
-            rdf.setData($service.config.instance);
-            var resource = rdf.secureByType('Configuration');
-
-            console.log('set', resource);
-
-            $scope.dialog.fileName = rdf.getString(resource, 'fileName');
-            $scope.dialog.sheetFilter = rdf.getString(resource, 'sheetFilter');
-            $scope.dialog.rowStart = rdf.getInteger(resource, 'rowStart');
-            $scope.dialog.rowEnd = rdf.getInteger(resource, 'rowEnd');
-            $scope.dialog.columnStart = rdf.getInteger(resource, 'columnStart');
-            $scope.dialog.columnEnd = rdf.getInteger(resource, 'columnEnd');
-            $scope.dialog.header = rdf.getBoolean(resource, 'header');
-            $scope.dialog.numericParse = rdf.getBoolean(resource, 'numericParse');
-            $scope.dialog.skipEmptyRows = rdf.getBoolean(resource, 'skipEmptyRows');
-            $scope.dialog.includeSheetName = rdf.getBoolean(resource, 'includeSheetName');
-        };
-
-        function saveDialog() {
-            var resource = rdf.secureByType('Configuration');
-
-            rdf.setString(resource, 'fileName', $scope.dialog.fileName);
-            rdf.setString(resource, 'sheetFilter', $scope.dialog.sheetFilter);
-            rdf.setInteger(resource, 'rowStart', $scope.dialog.rowStart);
-            rdf.setInteger(resource, 'rowEnd', $scope.dialog.rowEnd);
-            rdf.setInteger(resource, 'columnStart', $scope.dialog.columnStart);
-            rdf.setInteger(resource, 'columnEnd', $scope.dialog.columnEnd);
-            rdf.setBoolean(resource, 'header', $scope.dialog.header);
-            rdf.setBoolean(resource, 'numericParse', $scope.dialog.numericParse);
-            rdf.setBoolean(resource, 'skipEmptyRows', $scope.dialog.skipEmptyRows);
-            rdf.setBoolean(resource, 'includeSheetName', $scope.dialog.includeSheetName);
-
-            console.log('get', resource);
-
-            return rdf.getData();
-        };
-
-        // Define the save function.
-        $service.onStore = function () {
-            saveDialog();
+        if ($scope.dialog === undefined) {
+            $scope.dialog = {};
         }
 
-        // Load data.
-        loadDialog();
+        const dialogManager = $service.v1.manager(DESC, $scope.dialog);
+
+        $service.onStore = function () {
+            dialogManager.save();
+        };
+
+        dialogManager.load();
 
     }
-    //
-    controller.$inject = ['$scope', '$service', 'services.rdf.0.0.0'];
+
+    controller.$inject = ['$scope', '$service'];
     return controller;
 });
