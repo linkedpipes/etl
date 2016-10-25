64c17ef3abb114f4caf3d6b0973606405ca5d05d
Petr Škoda <skodapetr@gmail.com>
2016-10-25 10:42:36 +0200

    Add control support to t-tabularUv

--- plugins/t-tabularUv/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/t-tabularUv/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -1,238 +1,190 @@
 define([], function () {
-    function controller($scope, $service, rdfService) {
+    "use strict";
 
-        $scope.dialog = {
-            'keyColumn': '',
-            'baseUri': '',
-            'columns': [],
-            'advancedMapping': [],
-            'namedCell': [],
-            'quote': '',
-            'delimeter': '',
-            'linesToIgnore': 0,
-            'encoding': '',
-            'rowsLimit': '',
-            'tableType': '',
-            'hasHeader': true,
-            'generateNew': true,
-            'ignoreBlankCell': false,
-            'advancedKey': false,
-            'rowClass': '',
-            'sheetName': '',
-            'staticRowCounter': false,
-            'rowTriple': true,
-            'tableSubject': false,
-            'autoAsString': false,
-            'tableClass': false,
-            'generateLabels': false,
-            'stripHeader': false,
-            'trimString': false,
-            'xlsAdvancedParser': false,
-            'ignoreMissingColumn': false,
-            'generateRowTriple': false
-        };
-
-        $scope.control = { };
-
-        var rdf = rdfService.create('http://plugins.linkedpipes.com/ontology/t-tabularUv#');
-
-        function loadDialog() {
-            rdf.setData($service.config.instance);
-            var table = rdf.secureByType('Configuration');
-
-            $scope.dialog.keyColumn = rdf.getString(table, 'keyColumn');
-            $scope.dialog.baseUri = rdf.getString(table, 'baseUri');
-            $scope.dialog.quote = rdf.getString(table, 'quote');
-            $scope.dialog.delimeter = rdf.getString(table, 'delimeter');
-            $scope.dialog.linesToIgnore = rdf.getInteger(table, 'linesToIgnore');
-            $scope.dialog.encoding = rdf.getString(table, 'encoding');
-            $scope.dialog.rowsLimit = rdf.getInteger(table, 'rowsLimit');
-            $scope.dialog.tableType = rdf.getString(table, 'tableType');
-            $scope.dialog.hasHeader = rdf.getBoolean(table, 'hasHeader');
-            $scope.dialog.generateNew = rdf.getBoolean(table, 'generateNew');
-            $scope.dialog.ignoreBlankCell = rdf.getBoolean(table, 'ignoreBlankCell');
-            $scope.dialog.advancedKey = rdf.getBoolean(table, 'advancedKey');
-            $scope.dialog.rowTriple = rdf.getBoolean(table, 'rowTriple');
-            $scope.dialog.tableSubject = rdf.getBoolean(table, 'tableSubject');
-            $scope.dialog.rowClass = rdf.getString(table, 'rowsClass');
-            $scope.dialog.sheetName = rdf.getString(table, 'sheetName');
-            $scope.dialog.staticRowCounter = rdf.getBoolean(table, 'staticRowCounter');
-            $scope.dialog.rowTriple = rdf.getBoolean(table, 'rowTriple');
-            $scope.dialog.tableSubject = rdf.getBoolean(table, 'tableSubject');
-            $scope.dialog.autoAsString = rdf.getBoolean(table, 'autoAsString');
-            $scope.dialog.tableClass = rdf.getBoolean(table, 'tableClass');
-            $scope.dialog.generateLabels = rdf.getBoolean(table, 'generateLabels');
-            $scope.dialog.stripHeader = rdf.getBoolean(table, 'stripHeader');
-            $scope.dialog.trimString = rdf.getBoolean(table, 'trimString');
-            $scope.dialog.xlsAdvancedParser = rdf.getBoolean(table, 'xlsAdvancedParser');
-            $scope.dialog.ignoreMissingColumn = rdf.getBoolean(table, 'ignoreMissingColumn');
-            $scope.dialog.generateRowTriple = rdf.getBoolean(table, 'generateRowTriple');
-
-            var columns = rdf.getObjects(table, 'column');
-            var newItems = [];
-            columns.forEach(function(item) {
-                var columnRecord = {
-                    '@id' : item['@id'],
-                    'name': rdf.getString(item, 'name'),
-                    'uri': rdf.getString(item, 'uri'),
-                    'type': rdf.getString(item, 'type'),
-                    'typeFromDbf': rdf.getBoolean(item, 'typeFromDbf'),
-                    'lang': rdf.getString(item, 'lang')
-                };
-                newItems.push(columnRecord);
-            });
-            $scope.dialog.columns = newItems;
-
-            var advancedMapping = rdf.getObjects(table, 'columnsAdvanced');
-            var newItems = [];
-            advancedMapping.forEach(function(item) {
-                var columnRecord = {
-                    '@id' : item['@id'],
-                    'uri': rdf.getString(item, 'uri'),
-                    'template': rdf.getString(item, 'template')
-                };
-                newItems.push(columnRecord);
-            });
-            $scope.dialog.advancedMapping = newItems;
-
-            var namedCell = rdf.getObjects(table, 'namedCell');
-            var newItems = [];
-            namedCell.forEach(function(item) {
-                var columnRecord = {
-                    '@id' : item['@id'],
-                    'name': rdf.getString(item, 'name'),
-                    'rowNumber': rdf.getInteger(item, 'rowNumber'),
-                    'columnNumber': rdf.getInteger(item, 'columnNumber')
-                };
-                newItems.push(columnRecord);
-            });
-            $scope.dialog.namedCell = newItems;
-
-            $scope.control = $service.v0.iriToControl(
-                rdf.getIri(table, 'control'));
-        };
-
-        function saveDialog() {
-            if ($scope.control.forced) {
-                return;
+    const DESC = {
+        "$namespace": "http://plugins.linkedpipes.com/ontology/t-tabularUv#",
+        "$type": "Configuration",
+        "$options": {
+            "$predicate": "auto",
+            "$control": "auto"
+        },
+        "baseUri": {
+            "$type": "str",
+            "$label": "Key column"
+        },
+        "columns": {
+            "$label": "Mapping",
+            "$array": true,
+            "$object": {
+                "$type": "ColumnInfo",
+                "name" : {
+                    "$type": "str"
+                },
+                "type": {
+                    "$type": "str"
+                },
+                "lang": {
+                    "$type": "str"
+                },
+                "typeFromDbf": {
+                    "$type": "bool"
+                },
+                "uri" : {
+                    "$type": "str"
+                }
             }
-
-            var table = rdf.secureByType('Configuration');
-
-            rdf.setString(table, 'keyColumn', $scope.dialog.keyColumn);
-            rdf.setString(table, 'baseUri', $scope.dialog.baseUri);
-            rdf.setString(table, 'quote', $scope.dialog.quote);
-            rdf.setString(table, 'delimeter', $scope.dialog.delimeter);
-            rdf.setInteger(table, 'linesToIgnore', $scope.dialog.linesToIgnore);
-            rdf.setString(table, 'encoding', $scope.dialog.encoding);
-            rdf.setInteger(table, 'rowsLimit', $scope.dialog.rowsLimit);
-            rdf.setString(table, 'tableType', $scope.dialog.tableType);
-            rdf.setBoolean(table, 'hasHeader', $scope.dialog.hasHeader);
-            rdf.setBoolean(table, 'generateNew', $scope.dialog.generateNew);
-            rdf.setBoolean(table, 'ignoreBlankCell', $scope.dialog.ignoreBlankCell);
-            rdf.setBoolean(table, 'advancedKey', $scope.dialog.advancedKey);
-            rdf.setBoolean(table, 'rowTriple', $scope.dialog.rowTriple);
-            rdf.setBoolean(table, 'tableSubject', $scope.dialog.tableSubject);
-            rdf.setString(table, 'rowsClass', $scope.dialog.rowClass);
-            rdf.setString(table, 'sheetName', $scope.dialog.sheetName);
-            rdf.setBoolean(table, 'staticRowCounter', $scope.dialog.staticRowCounter);
-            rdf.setBoolean(table, 'rowTriple', $scope.dialog.rowTriple);
-            rdf.setBoolean(table, 'tableSubject', $scope.dialog.tableSubject);
-            rdf.setBoolean(table, 'autoAsString', $scope.dialog.autoAsString);
-            rdf.setBoolean(table, 'tableClass', $scope.dialog.tableClass);
-            rdf.setBoolean(table, 'generateLabels', $scope.dialog.generateLabels);
-            rdf.setBoolean(table, 'stripHeader', $scope.dialog.stripHeader);
-            rdf.setBoolean(table, 'trimString', $scope.dialog.trimString);
-            rdf.setBoolean(table, 'xlsAdvancedParser', $scope.dialog.xlsAdvancedParser);
-            rdf.setBoolean(table, 'ignoreMissingColumn', $scope.dialog.ignoreMissingColumn);
-            rdf.setBoolean(table, 'generateRowTriple', $scope.dialog.generateRowTriple);
-
-            var items = [];
-            $scope.dialog.columns.forEach(function(item) {
-                var id = item['@id'];
-                var record;
-                if (id) {
-                    // Try to read existing object.
-                    record = rdf.filterSingle(rdf.findByUri(id));
+        },
+        "advancedMapping": {
+            "$label": "Advanced mapping",
+            "$array": true,
+            "$object": {
+                "$type": "AdvancedMapping",
+                "uri" : {
+                    "$type": "str"
+                },
+                "template" : {
+                    "$type": "str"
                 }
-                if (!id || !record) {
-                    // New object.
-                    record = rdf.createObject('ColumnInfo');
+            }
+        },
+        "namedCell": {
+            "$label": "XLS cell mapping",
+            "$array": true,
+            "$object": {
+                "$type": "NamedCell",
+                "name" : {
+                    "$type": "str"
+                },
+                "rowNumber" : {
+                    "$type": "int"
+                },
+                "columnNumber" : {
+                    "$type": "int"
                 }
-                // Now we have object to store data into.
-                rdf.setString(record, 'name', item['name']);
-                rdf.setString(record, 'uri', item['uri']);
-                rdf.setString(record, 'type', item['type']);
-                rdf.setBoolean(record, 'typeFromDbf', item['typeFromDbf']);
-                rdf.setString(record, 'lang', item['lang']);
-                items.push(record);
-            });
-            rdf.updateObjects(table, 'column', items, false);
+            }
+        },
+        "quote": {
+            "$type": "str",
+            "$label": "Quote char"
+        },
+        "delimeter": {
+            "$type": "str",
+            "$label": "Delimiter char"
+        },
+        "linesToIgnore": {
+            "$type": "int",
+            "$label": "Skip n first lines"
+        },
+        "encoding": {
+            "$type": "str",
+            "$label": "Encoding"
+        },
+        "rowsLimit": {
+            "$type": "str",
+            "$label": "Rows limit"
+        },
+        "tableType": {
+            "$type": "str",
+            "$label": "Table type"
+        },
+        "hasHeader": {
+            "$type": "bool",
+            "$label": "Has header"
+        },
+        "generateNew": {
+            "$type": "bool",
+            "$label": "Full column mapping"
+        },
+        "ignoreBlankCell": {
+            "$type": "bool",
+            "$label": "Ignore blank cells"
+        },
+        "advancedKey": {
+            "$type": "bool",
+            "$label": "Advances key column"
+        },
+        "rowClass": {
+            "$type": "str",
+            "$label": "Class for a row entity"
+        },
+        "sheetName": {
+            "$type": "str",
+            "$label": "Sheet name"
+        },
+        "staticRowCounter": {
+            "$type": "bool",
+            "$label": "Use static row counter"
+        },
+        "rowTriple": {
+            "$type": "bool",
+            "$label": "Generate row column"
+        },
+        "tableSubject": {
+            "$type": "bool",
+            "$label": "Generate subject for table"
+        },
+        "autoAsString": {
+            "$type": "bool",
+            "$label": "Auto type as string"
+        },
+        "tableClass": {
+            "$type": "bool",
+            "$label": "Generate table/row class"
+        },
+        "generateLabels": {
+            "$type": "bool",
+            "$label": "Generate labels"
+        },
+        "stripHeader": {
+            "$type": "bool",
+            "$label": "Strip header for nulls"
+        },
+        "trimString": {
+            "$type": "bool",
+            "$label": "Remove trailing spaces"
+        },
+        "xlsAdvancedParser": {
+            "$type": "bool",
+            "$label": "Use advanced parser for 'double'"
+        },
+        "ignoreMissingColumn": {
+            "$type": "bool",
+            "$label": "Ignore missing columns"
+        },
+        "generateRowTriple": {
+            "$type": "bool",
+            "$label": "Generate row column"
+        }
+    };
 
-            items = [];
-            $scope.dialog.advancedMapping.forEach(function(item) {
-                var id = item['@id'];
-                var record;
-                if (id) {
-                    // Try to read existing object.
-                    record = rdf.filterSingle(rdf.findByUri(id));
-                }
-                if (!id || !record) {
-                    // New object.
-                    record = rdf.createObject('AdvancedMapping');
-                }
-                // Now we have object to store data into.
-                rdf.setString(record, 'uri', item['uri']);
-                rdf.setString(record, 'template', item['template']);
-                items.push(record);
-            });
-            rdf.updateObjects(table, 'columnsAdvanced', items, false);
+    function controller($scope, $service) {
 
-            items = [];
-            $scope.dialog.namedCell.forEach(function(item) {
-                var id = item['@id'];
-                var record;
-                if (id) {
-                    // Try to read existing object.
-                    record = rdf.filterSingle(rdf.findByUri(id));
-                }
-                if (!id || !record) {
-                    // New object.
-                    record = rdf.createObject('NamedCell');
-                }
-                // Now we have object to store data into.
-                rdf.setString(record, 'name', item['name']);
-                rdf.setInteger(record, 'rowNumber', item['rowNumber']);
-                rdf.setInteger(record, 'columnNumber', item['columnNumber']);
-                items.push(record);
-            });
-            rdf.updateObjects(table, 'namedCell', items, false);
-
-            return rdf.getData();
-        };
+        if ($scope.dialog === undefined) {
+            $scope.dialog = {};
+        }
 
         $scope.onAddColumn = function () {
-            $scope.dialog.columns.push({
-                'name': '',
-                'type': '',
-                'lang': '',
-                'typeFromDbf' : false,
-                'uri': ''
+            $scope.dialog.columns.value.push({
+                'name': {'value': ''},
+                'type': {'value': ''},
+                'lang': {'value': ''},
+                'typeFromDbf': {'value': false},
+                'uri': {'value': ''}
             });
         };
 
         $scope.onAddAdvancedColumn = function () {
-            $scope.dialog.advancedMapping.push({
-                'uri': '',
-                'template': ''
+            $scope.dialog.advancedMapping.value.push({
+                'uri': {'value': ''},
+                'template': {'value': ''}
             });
         };
 
         $scope.onAddNamedCell = function () {
-            $scope.dialog.namedCell.push({
-                'name': '',
-                'rowNumber': 0,
-                'columnNumber': 0
+            $scope.dialog.namedCell.value.push({
+                'name': {'value': ''},
+                'rowNumber': {'value': 0},
+                'columnNumber': {'value': 0}
             });
         };
 
@@ -240,15 +192,16 @@ define([], function () {
             data.splice(index, 1);
         };
 
-        // Define the save function.
-        $service.onStore = function () {
-            saveDialog();
-        }
+        const dialogManager = $service.v1.manager(DESC, $scope.dialog);
 
-        // Load data.
-        loadDialog();
+        $service.onStore = function () {
+            dialogManager.save();
+        };
+
+        dialogManager.load();
+
     }
-    //
-    controller.$inject = ['$scope', '$service', 'services.rdf.0.0.0'];
+
+    controller.$inject = ['$scope', '$service'];
     return controller;
 });
