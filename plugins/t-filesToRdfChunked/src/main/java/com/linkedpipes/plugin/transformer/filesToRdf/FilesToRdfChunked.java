57a197be780fd3531893160d468958618f91fc8b
Petr Škoda <skodapetr@gmail.com>
2016-11-08 15:18:57 +0100

    Add t-filesToRdfChunked

---
+++ plugins/t-filesToRdfChunked/src/main/java/com/linkedpipes/plugin/transformer/filesToRdf/FilesToRdfChunked.java
@@ -0,0 +1,98 @@
+package com.linkedpipes.plugin.transformer.filesToRdf;
+
+import com.linkedpipes.etl.component.api.Component;
+import com.linkedpipes.etl.component.api.service.ExceptionFactory;
+import com.linkedpipes.etl.component.api.service.ProgressReport;
+import com.linkedpipes.etl.dataunit.sesame.api.rdf.WritableChunkedStatements;
+import com.linkedpipes.etl.dataunit.system.api.files.FilesDataUnit;
+import com.linkedpipes.etl.executor.api.v1.exception.LpException;
+import org.openrdf.model.Statement;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.AbstractRDFHandler;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public final class FilesToRdfChunked implements Component.Sequential {
+
+    @Component.InputPort(id = "InputFiles")
+    public FilesDataUnit inputFiles;
+
+    @Component.OutputPort(id = "OutputRdf")
+    public WritableChunkedStatements outputRdf;
+
+    @Component.Configuration
+    public FilesToRdfConfiguration configuration;
+
+    @Component.Inject
+    public ProgressReport progressReport;
+
+    @Component.Inject
+    public ExceptionFactory exceptionFactory;
+
+    /**
+     * Buffer used to store data.
+     */
+    private List<Statement> buffer = new ArrayList<>(10000);
+
+    @Override
+    public void execute() throws LpException {
+        final RDFFormat defaultFormat;
+        if (configuration.getMimeType() == null
+                || configuration.getMimeType().isEmpty()) {
+            defaultFormat = null;
+        } else {
+            final Optional<RDFFormat> optionalFormat
+                    = Rio.getParserFormatForMIMEType(
+                    configuration.getMimeType());
+            if (optionalFormat.isPresent()) {
+                defaultFormat = optionalFormat.get();
+            } else {
+                defaultFormat = null;
+            }
+        }
+        //
+        progressReport.start(inputFiles.size());
+        for (FilesDataUnit.Entry entry : inputFiles) {
+            if (defaultFormat == null) {
+                final RDFFormat format = Rio.getParserFormatForFileName(
+                        entry.getFileName()).orElseGet(null);
+                if (format == null) {
+                    throw exceptionFactory.failure(
+                            "Can't determine format for file: {}",
+                            entry.getFileName());
+                }
+                loadFile(entry.toFile(), format);
+            } else {
+                loadFile(entry.toFile(), defaultFormat);
+            }
+            progressReport.entryProcessed();
+        }
+        progressReport.done();
+    }
+
+    private void loadFile(File file, RDFFormat format) throws LpException {
+        buffer.clear();
+        try (InputStream stream = new FileInputStream(file)) {
+            final RDFParser parser = Rio.createParser(format);
+            parser.setRDFHandler(new AbstractRDFHandler() {
+                @Override
+                public void handleStatement(Statement st) {
+                    buffer.add(st);
+                }
+            });
+            parser.parse(stream, "http://localhost/base/");
+        } catch (IOException ex) {
+            exceptionFactory.failure("Can't load file: {}", file, ex);
+        }
+        outputRdf.submit(buffer);
+    }
+
+}
---
+++ plugins/t-filesToRdfChunked/src/main/java/com/linkedpipes/plugin/transformer/filesToRdf/FilesToRdfConfiguration.java
@@ -0,0 +1,32 @@
+package com.linkedpipes.plugin.transformer.filesToRdf;
+
+import com.linkedpipes.etl.component.api.service.RdfToPojo;
+
+@RdfToPojo.Type(uri = FilesToRdfVocabulary.CONFIG)
+public class FilesToRdfConfiguration {
+
+    @RdfToPojo.Property(uri = FilesToRdfVocabulary.HAS_MIME_TYPE)
+    private String mimeType = null;
+
+    @RdfToPojo.Property(uri = FilesToRdfVocabulary.HAS_CHUNK_SIZE)
+    private int filesPerChunk = 1;
+
+    public FilesToRdfConfiguration() {
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public int getFilesPerChunk() {
+        return filesPerChunk;
+    }
+
+    public void setFilesPerChunk(int filesPerChunk) {
+        this.filesPerChunk = filesPerChunk;
+    }
+}
