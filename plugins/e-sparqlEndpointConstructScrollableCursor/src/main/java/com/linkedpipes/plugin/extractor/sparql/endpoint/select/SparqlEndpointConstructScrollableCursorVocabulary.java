d6c18eaa4311dd38e883acdd2a6c1b0b952db5a8
Petr Škoda <skodapetr@gmail.com>
2016-11-15 12:00:23 +0100

    Add e-sparqlEndpointConstructScrollableCursor

---
+++ plugins/e-sparqlEndpointConstructScrollableCursor/src/main/java/com/linkedpipes/plugin/extractor/sparql/endpoint/select/SparqlEndpointConstructScrollableCursorVocabulary.java
@@ -0,0 +1,25 @@
+package com.linkedpipes.plugin.extractor.sparql.endpoint.select;
+
+final class SparqlEndpointConstructScrollableCursorVocabulary {
+
+    private static final String PREFIX
+            = "http://plugins.linkedpipes.com/ontology/e-sparqlEndpointConstructScrollableCursor#";
+
+    public static final String CONFIG = PREFIX + "Configuration";
+
+    public static final String HAS_OUTER_CONSTRUCT = PREFIX + "outerConstruct";
+
+    public static final String HAS_PREFIXES = PREFIX + "prefixes";
+
+    public static final String HAS_INNER_SELECT = PREFIX + "innerSelect";
+
+    public static final String HAS_ENDPOINT = PREFIX + "endpoint";
+
+    public static final String HAS_DEFAULT_GRAPH = PREFIX + "defaultGraph";
+
+    public static final String HAS_SELECT_SIZE = PREFIX + "selectSize";
+
+    private SparqlEndpointConstructScrollableCursorVocabulary() {
+    }
+
+}
