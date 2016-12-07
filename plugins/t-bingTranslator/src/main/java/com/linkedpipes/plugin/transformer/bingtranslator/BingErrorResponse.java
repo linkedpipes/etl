8fd11c0e2aa6770108962e85e6f63850d9a0a6fc
Petr Škoda <skodapetr@gmail.com>
2016-12-07 13:40:25 +0100

    Update t-bingTranslator
    Add comments and definition of private configuration fields.

--- plugins/t-bingTranslator/src/main/java/com/linkedpipes/plugin/transformer/bingtranslator/BingErrorResponse.java
+++ plugins/t-bingTranslator/src/main/java/com/linkedpipes/plugin/transformer/bingtranslator/BingErrorResponse.java
@@ -1,5 +1,8 @@
 package com.linkedpipes.plugin.transformer.bingtranslator;
 
+/**
+ * Definition of handled error responses from Bing translation API.
+ */
 public final class BingErrorResponse {
 
     public static final String TOO_MANY_ELEMENTS
