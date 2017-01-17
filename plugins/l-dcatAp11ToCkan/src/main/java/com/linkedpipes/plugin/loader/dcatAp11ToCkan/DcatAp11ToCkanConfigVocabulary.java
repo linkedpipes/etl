bafb2df31785efcb0238513e3961779247d561d6
Jakub Klímek <klimek@ksi.mff.cuni.cz>
2017-01-17 09:34:58 +0100

    Feature/l-dcatap11tockan (#294)
    * Upgrade l-dcatAp11toCKAN
    
    * Fix case in .java files
    
    * Fix case in .java files
    
    * Update l-dcatAp11ToCkan
    
    Add configuration control support.
    
    * Fix l-dcatAp11ToCkan

--- plugins/l-dcatAp11ToCkan/src/main/java/com/linkedpipes/plugin/loader/dcatAp11ToCkan/DcatAp11ToCkanConfigVocabulary.java
+++ plugins/l-dcatAp11ToCkan/src/main/java/com/linkedpipes/plugin/loader/dcatAp11ToCkan/DcatAp11ToCkanConfigVocabulary.java
@@ -4,7 +4,7 @@ package com.linkedpipes.plugin.loader.dcatAp11ToCkan;
  *
  * @author Klímek Jakub
  */
-public final class DcatAp11ToCkanConfigVocabulary {
+public class DcatAp11ToCkanConfigVocabulary {
 
     private static final String PREFIX = "http://plugins.linkedpipes.com/ontology/l-dcatAp11ToCkan#";
 
@@ -14,14 +14,10 @@ public final class DcatAp11ToCkanConfigVocabulary {
 
     public static final String API_KEY = PREFIX + "apiKey";
 
-    public static final String CREATE_CKAN_ORG = PREFIX + "createCkanOrg";
-
     public static final String DATASET_ID = PREFIX + "datasetID";
-
-    public static final String ORG_ID = PREFIX + "organizationId";
 
     public static final String LOAD_LANGUAGE = PREFIX + "loadLanguage";
 
-    public static final String OVERWRITE = PREFIX + "overwrite";
+    public static final String PROFILE = PREFIX + "profile";
 
 }
