4914bf0c99584c14285b17d90a2366c8ec609b34
Jakub Klímek <klimek@ksi.mff.cuni.cz>
2017-02-23 15:57:59 +0100

    Add l-lodCloud

---
+++ plugins/l-lodCloud/src/main/java/com/linkedpipes/plugin/loader/lodCloud/LodCloudConfigVocabulary.java
@@ -0,0 +1,14 @@
+package com.linkedpipes.plugin.loader.lodCloud;
+
+public final class LodCloudConfigVocabulary {
+
+    private static final String PREFIX =
+            "http://plugins.linkedpipes.com/ontology/l-lodCloud#";
+
+    public static final String CONFIG_CLASS = PREFIX + "Configuration";
+
+    public static final String API_KEY = PREFIX + "apiKey";
+
+    public static final String DATASET_ID = PREFIX + "datasetID";
+
+}
---
+++ plugins/l-lodCloud/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -0,0 +1,83 @@
+define([], function () {
+    "use strict";
+
+    const DESC = {
+        "$namespace" :
+            "http://plugins.linkedpipes.com/ontology/l-lodCloud#",
+        "$type": "Configuration",
+        "$options" : {
+            "$predicate": "auto",
+            "$control": "auto"
+        },
+        "apiUrl" : {
+            "$type" : "str",
+            "$label" : "CKAN Action API URL"
+        },
+        "apiKey" : {
+            "$type" : "str",
+            "$label" : "CKAN API Key"
+        },
+        "datasetID" : {
+            "$type" : "str",
+            "$label" : "CKAN dataset ID"
+        },
+        "loadLanguage" : {
+            "$type" : "str",
+            "$label" : "Load language (cs|en)"
+        },
+        "voidExampleResources" : {
+            "$type" : "bool",
+            "$label" : "Generate CKAN resources from VoID example resources"
+        },
+        "voidSparqlEndpoint" : {
+            "$type" : "bool",
+            "$label" : "Generate CKAN resource from VoID SPARQL endpoint"
+        },
+        "profile" : {
+            "$type": "str",
+            "$label": "CKAN profile"
+        }
+    };
+
+    const PROFILES = [
+        {
+            "@id": "http://plugins.etl.linkedpipes.com/resource/l-lodCloud/profiles/CKAN",
+            "http://www.w3.org/2004/02/skos/core#prefLabel": [
+                {
+                    "@language": "en",
+                    "@value": "Pure CKAN"
+                }
+            ]
+        },
+        {
+            "@id": "http://plugins.etl.linkedpipes.com/resource/l-lodCloud/profiles/CZ-NKOD",
+            "http://www.w3.org/2004/02/skos/core#prefLabel": [
+                {
+                    "@language": "en",
+                    "@value": "Czech NKOD"
+                }
+            ]
+        }
+    ];
+
+    function controller($scope, $service) {
+
+        if ($scope.dialog === undefined) {
+            $scope.dialog = {};
+        }
+
+        const dialogManager = $service.v1.manager(DESC, $scope.dialog);
+
+        $service.onStore = function () {
+            dialogManager.save();
+        };
+
+        $scope.profiles = PROFILES;
+
+        dialogManager.load();
+
+    }
+
+    controller.$inject = ['$scope', '$service'];
+    return controller;
+});
