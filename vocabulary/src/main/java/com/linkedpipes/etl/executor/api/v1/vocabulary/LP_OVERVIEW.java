7b156421e00e9be17260e57ff640ae0c221cfc80
Petr Škoda <skodapetr@gmail.com>
2017-02-24 09:14:26 +0100

    Update vocabulary used by overview

--- vocabulary/src/main/java/com/linkedpipes/etl/executor/api/v1/vocabulary/LP_OVERVIEW.java
+++ vocabulary/src/main/java/com/linkedpipes/etl/executor/api/v1/vocabulary/LP_OVERVIEW.java
@@ -9,19 +9,21 @@ public class LP_OVERVIEW {
 
     public static final String HAS_EXECUTION = PREFIX + "execution";
 
-    public static final String HAS_START = PREFIX + "start";
+    public static final String HAS_START = PREFIX + "executionStarted";
 
-    public static final String HAS_END = PREFIX + "end";
+    public static final String HAS_END = PREFIX + "executionEnded";
 
-    public static final String HAS_STATUS = PREFIX + "status";
+    public static final String HAS_STATUS = PREFIX + "executionStatus";
 
     public static final String HAS_LAST_CHANGE = PREFIX + "lastChange";
 
     public static final String HAS_PIPELINE_PROGRESS =
             PREFIX + "pipelineProgress";
 
-    public static final String HAS_PROGRESS_TOTAL = PREFIX + "total";
+    public static final String HAS_PROGRESS_TOTAL = PREFIX +
+            "numberOfExecutableComponents";
 
-    public static final String HAS_PROGRESS_CURRENT = PREFIX + "current";
+    public static final String HAS_PROGRESS_CURRENT = PREFIX +
+            "numberOfExecutedComponents";
 
 }
