6a02e4d6dca60d4d342d448f07f6f7e17aca9c1b
Petr Škoda <skodapetr@gmail.com>
2017-03-02 11:18:48 +0100

    Add l-ftpFiles component

---
+++ plugins/l-ftpFiles/src/main/java/com/linkedpipes/plugin/loader/ftpfiles/FtpFilesLoaderVocabulary.java
@@ -0,0 +1,23 @@
+package com.linkedpipes.plugin.loader.ftpfiles;
+
+public final class FtpFilesLoaderVocabulary {
+
+    private FtpFilesLoaderVocabulary() {
+    }
+
+    private static final String PREFIX
+            = "http://plugins.linkedpipes.com/ontology/l-ftpFiles#";
+
+    public static final String CONFIG = PREFIX + "Configuration";
+
+    public static final String HAS_USERNAME = PREFIX + "username";
+
+    public static final String HAS_PASSWORD = PREFIX + "password";
+
+    public static final String HAS_HOST = PREFIX + "host";
+
+    public static final String HAS_PORT = PREFIX + "port";
+
+    public static final String HAS_DIRECTORY = PREFIX + "directory";
+
+}
