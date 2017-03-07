792e75d092a9cae055a9ed1e924e1626d4f3f3c5
Petr Škoda <skodapetr@gmail.com>
2017-03-07 18:28:47 +0100

    Add t-jsonLdFormat component
    #308

---
+++ plugins/t-jsonLdFormat/src/main/java/com/linkedpipes/plugin/transformer/jsonldformat/JsonLdFormat.java
@@ -0,0 +1,115 @@
+package com.linkedpipes.plugin.transformer.jsonldformat;
+
+import com.github.jsonldjava.core.JsonLdError;
+import com.github.jsonldjava.core.JsonLdOptions;
+import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.utils.JsonUtils;
+import com.linkedpipes.etl.dataunit.core.files.FilesDataUnit;
+import com.linkedpipes.etl.dataunit.core.files.WritableFilesDataUnit;
+import com.linkedpipes.etl.executor.api.v1.LpException;
+import com.linkedpipes.etl.executor.api.v1.component.Component;
+import com.linkedpipes.etl.executor.api.v1.component.SequentialExecution;
+import com.linkedpipes.etl.executor.api.v1.service.ExceptionFactory;
+import com.linkedpipes.etl.executor.api.v1.service.ProgressReport;
+
+import java.io.*;
+
+public class JsonLdFormat implements Component, SequentialExecution {
+
+    @Component.InputPort(iri = "InputFiles")
+    public FilesDataUnit inputFiles;
+
+    @Component.InputPort(iri = "OutputFiles")
+    public WritableFilesDataUnit outputFiles;
+
+    @Component.Configuration
+    public JsonLdFormatConfiguration configuration;
+
+    @Component.Inject
+    public ExceptionFactory exceptionFactory;
+
+    @Component.Inject
+    public ProgressReport progressReport;
+
+    private Object context;
+
+    @Override
+    public void execute() throws LpException {
+        initializeContext();
+        progressReport.start(inputFiles.size());
+        for (FilesDataUnit.Entry entry : inputFiles) {
+            File inputFIle = entry.toFile();
+            File outputFile = outputFiles.createFile(entry.getFileName());
+            try {
+                transformFile(inputFIle, outputFile);
+            } catch (LpException ex) {
+                throw exceptionFactory.failure("Can't transform: {}",
+                        entry.getFileName(), ex);
+            }
+            progressReport.entryProcessed();
+        }
+        progressReport.done();
+    }
+
+    private void initializeContext() throws LpException {
+        if (JsonLdFormatVocabulary.EXPANDED.equals(configuration.getFormat())) {
+            // Expanded mode does not use context.
+            return;
+        }
+        try {
+            context = JsonUtils.fromString(configuration.getContext());
+        } catch(IOException ex) {
+            throw exceptionFactory.failure("Can't prepare context.", ex);
+        }
+    }
+
+    private void transformFile(File source, File target) throws LpException {
+        final Object inputObject = readJsonLdFile(source);
+        final Object outputObject = transformJsonLd(inputObject);
+        writeJsonLdFile(target, outputObject);
+    }
+
+    private Object readJsonLdFile(File file) throws LpException {
+        try (InputStream input = new FileInputStream(file)) {
+            return JsonUtils.fromInputStream(input);
+        } catch (IOException ex) {
+            throw exceptionFactory.failure("Can't open input file: {}",
+                    file, ex);
+        }
+    }
+
+    private Object transformJsonLd(Object object) throws LpException {
+        JsonLdOptions options = new JsonLdOptions();
+        try {
+            return formatJsonLd(object, options);
+        } catch (JsonLdError ex) {
+            throw exceptionFactory.failure("Can't transform object.", ex);
+        }
+    }
+
+    private void writeJsonLdFile(File file, Object jsonLdObject)
+            throws LpException {
+        file.getParentFile().mkdirs();
+        try (FileWriter writer = new FileWriter(file)) {
+            JsonUtils.write(writer, jsonLdObject);
+        } catch (IOException ex) {
+            throw exceptionFactory.failure("Can't write file: {}", file, ex);
+        }
+    }
+
+    private Object formatJsonLd(Object object,
+            JsonLdOptions options) throws LpException, JsonLdError {
+        switch (configuration.getFormat()) {
+            case JsonLdFormatVocabulary.COMPACT:
+                return JsonLdProcessor.compact(object, context, options);
+            case JsonLdFormatVocabulary.FLAT:
+                return JsonLdProcessor.flatten(object, context, options);
+            case JsonLdFormatVocabulary.EXPANDED:
+                return JsonLdProcessor.expand(object, options);
+            default:
+                throw exceptionFactory.failure("Invalid format type: '{}'",
+                        configuration.getFormat());
+        }
+    }
+
+}
---
+++ plugins/t-jsonLdFormat/src/main/java/com/linkedpipes/plugin/transformer/jsonldformat/JsonLdFormatVocabulary.java
@@ -0,0 +1,23 @@
+package com.linkedpipes.plugin.transformer.jsonldformat;
+
+public final class JsonLdFormatVocabulary {
+
+    private static final String PREFIX
+            = "http://plugins.linkedpipes.com/ontology/t-jsonLdFormat#";
+
+    public static final String CONFIGURATION = PREFIX + "Configuration";
+
+    public static final String HAS_FORMAT = PREFIX + "format";
+
+    public static final String HAS_CONTEXT = PREFIX + "context";
+
+    public static final String FLAT = PREFIX + "Flat";
+
+    public static final String EXPANDED = PREFIX + "Expanded";
+
+    public static final String COMPACT = PREFIX + "Compact";
+
+    private JsonLdFormatVocabulary() {
+    }
+
+}
