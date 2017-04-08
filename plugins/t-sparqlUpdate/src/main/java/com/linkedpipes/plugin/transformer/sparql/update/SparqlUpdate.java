640658ed2477324299b4d2b2f9bef854ba001dd1
Petr Škoda <skodapetr@gmail.com>
2017-04-08 21:44:07 +0200

    Update t-sparqlUpdate
    Enable usage of different input and output repository.

--- plugins/t-sparqlUpdate/src/main/java/com/linkedpipes/plugin/transformer/sparql/update/SparqlUpdate.java
+++ plugins/t-sparqlUpdate/src/main/java/com/linkedpipes/plugin/transformer/sparql/update/SparqlUpdate.java
@@ -6,16 +6,14 @@ import com.linkedpipes.etl.executor.api.v1.LpException;
 import com.linkedpipes.etl.executor.api.v1.component.Component;
 import com.linkedpipes.etl.executor.api.v1.component.SequentialExecution;
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.query.Dataset;
 import org.eclipse.rdf4j.query.QueryLanguage;
 import org.eclipse.rdf4j.query.Update;
 import org.eclipse.rdf4j.query.impl.SimpleDataset;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.rdf4j.repository.RepositoryResult;
 
 public final class SparqlUpdate implements Component, SequentialExecution {
-
-    private static final Logger LOG =
-            LoggerFactory.getLogger(SparqlUpdate.class);
 
     @Component.InputPort(iri = "InputRdf")
     public SingleGraphDataUnit inputRdf;
@@ -32,32 +30,42 @@ public final class SparqlUpdate implements Component, SequentialExecution {
 
     @Override
     public void execute() throws LpException {
-        final IRI inputGraph = inputRdf.getReadGraph();
-        final IRI outputGraph = outputRdf.getWriteGraph();
-        LOG.info("Update: {} -> {}", inputGraph, outputGraph);
-        LOG.info("Query: {}", configuration.getQuery());
-        // Copy data.
-        inputRdf.execute((connection) -> {
-            connection.add(connection
-                            .getStatements(null, null, null, true, inputGraph),
-                    outputGraph);
-            LOG.info("Input size: {}", connection.size(outputGraph));
-        });
-        // Perform update.
-        inputRdf.execute((connection) -> {
-            final Update update = connection.prepareUpdate(QueryLanguage.SPARQL,
-                    configuration.getQuery());
-            final SimpleDataset dataset = new SimpleDataset();
-            dataset.addDefaultGraph(outputGraph);
-            dataset.addDefaultRemoveGraph(outputGraph);
-            dataset.setDefaultInsertGraph(outputGraph);
-            update.setDataset(dataset);
-            update.execute();
-        });
-        inputRdf.execute((connection) -> {
-            LOG.info("Output size: {}", connection.size(outputGraph));
-        });
-
+        copyStatements();
+        executeUpdate();
     }
 
+    private void copyStatements() throws LpException {
+        IRI inputGraph = inputRdf.getReadGraph();
+        inputRdf.execute((connection) -> {
+            RepositoryResult<Statement> statement = connection
+                    .getStatements(null, null, null, true, inputGraph);
+            addToOutput(statement);
+        });
+    }
+
+    private void addToOutput(RepositoryResult<Statement> statement)
+            throws LpException {
+        IRI outputGraph = outputRdf.getWriteGraph();
+        outputRdf.execute((connection) -> {
+            connection.add(statement, outputGraph);
+        });
+    }
+
+    private void executeUpdate() throws LpException {
+        outputRdf.execute((connection) -> {
+            Update update = connection.prepareUpdate(QueryLanguage.SPARQL,
+                    configuration.getQuery());
+            update.setDataset(createDataset());
+            update.execute();
+        });
+    }
+
+    private Dataset createDataset() {
+        IRI outputGraph = outputRdf.getWriteGraph();
+        SimpleDataset dataset = new SimpleDataset();
+        dataset.addDefaultGraph(outputGraph);
+        dataset.addDefaultRemoveGraph(outputGraph);
+        dataset.setDefaultInsertGraph(outputGraph);
+        return dataset;
+    }
 }
