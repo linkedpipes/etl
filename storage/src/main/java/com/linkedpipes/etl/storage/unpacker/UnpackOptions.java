4202096147068722e90e97c7ae0c80df0c393bf3
Petr Škoda <skodapetr@gmail.com>
2017-04-12 20:58:55 +0200

    Update storage to improve testability

--- storage/src/main/java/com/linkedpipes/etl/storage/unpacker/UnpackOptions.java
+++ storage/src/main/java/com/linkedpipes/etl/storage/unpacker/UnpackOptions.java
@@ -1,5 +1,8 @@
 package com.linkedpipes.etl.storage.unpacker;
 
+import com.linkedpipes.etl.rdf.utils.RdfUtilsException;
+import com.linkedpipes.etl.rdf.utils.model.RdfValue;
+import com.linkedpipes.etl.rdf.utils.pojo.Loadable;
 import com.linkedpipes.etl.storage.rdf.PojoLoader;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Value;
@@ -13,7 +16,7 @@ import java.util.List;
 /**
  * Describe options that can be used to modify pipeline unpacking.
  */
-class UnpackOptions implements PojoLoader.Loadable {
+class UnpackOptions implements PojoLoader.Loadable, Loadable {
 
     public static final IRI TYPE;
 
@@ -23,7 +26,8 @@ class UnpackOptions implements PojoLoader.Loadable {
                 "http://etl.linkedpipes.com/ontology/ExecutionOptions");
     }
 
-    public static class ComponentMapping implements PojoLoader.Loadable {
+    public static class ComponentMapping
+            implements PojoLoader.Loadable, Loadable {
 
         private String source;
 
@@ -43,6 +47,20 @@ class UnpackOptions implements PojoLoader.Loadable {
             return null;
         }
 
+        @Override
+        public Loadable load(String predicate, RdfValue value)
+                throws RdfUtilsException {
+            switch (predicate) {
+                case "http://etl.linkedpipes.com/ontology/mappingSource":
+                    source = value.asString();
+                    break;
+                case "http://etl.linkedpipes.com/ontology/mappingTarget":
+                    target = value.asString();
+                    break;
+            }
+            return null;
+        }
+
         public String getSource() {
             return source;
         }
@@ -52,7 +70,8 @@ class UnpackOptions implements PojoLoader.Loadable {
         }
     }
 
-    public static class ExecutionMapping implements PojoLoader.Loadable {
+    public static class ExecutionMapping
+            implements PojoLoader.Loadable, Loadable {
 
         private String execution;
 
@@ -72,6 +91,23 @@ class UnpackOptions implements PojoLoader.Loadable {
                 default:
                     return null;
             }
+        }
+
+        @Override
+        public Loadable load(String predicate, RdfValue value)
+                throws RdfUtilsException {
+            switch (predicate) {
+                case "http://etl.linkedpipes.com/ontology/execution":
+                    execution = value.asString();
+                    return null;
+                case "http://etl.linkedpipes.com/ontology/mapping":
+                    final ComponentMapping mapping = new ComponentMapping();
+                    components.add(mapping);
+                    return mapping;
+                default:
+                    return null;
+            }
+
         }
 
         public String getExecution() {
@@ -109,6 +145,28 @@ class UnpackOptions implements PojoLoader.Loadable {
             case "http://linkedpipes.com/ontology/deleteWorkingData":
                 deleteWorkingDirectory = "true".equals(
                         value.stringValue().toLowerCase());
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public Loadable load(String predicate, RdfValue value)
+            throws RdfUtilsException {
+        switch (predicate) {
+            case "http://etl.linkedpipes.com/ontology/runTo":
+                runToComponent = value.asString();
+                return null;
+            case "http://etl.linkedpipes.com/ontology/executionMapping":
+                final ExecutionMapping mapping = new ExecutionMapping();
+                executionMapping.add(mapping);
+                return mapping;
+            case "http://linkedpipes.com/ontology/saveDebugData":
+                saveDebugData = value.asBoolean();
+                return null;
+            case "http://linkedpipes.com/ontology/deleteWorkingData":
+                deleteWorkingDirectory = value.asBoolean();
                 return null;
             default:
                 return null;
