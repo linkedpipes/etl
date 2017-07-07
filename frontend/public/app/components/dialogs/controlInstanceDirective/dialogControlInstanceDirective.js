1d8d235fea2e08cb8a254177d7de0d5742da4a0a
Petr Škoda <skodapetr@gmail.com>
2017-07-07 22:17:36 +0200

    Show message if all component options are forced
    #399

--- frontend/public/app/components/dialogs/controlInstanceDirective/dialogControlInstanceDirective.js
+++ frontend/public/app/components/dialogs/controlInstanceDirective/dialogControlInstanceDirective.js
@@ -3,23 +3,41 @@ define([], function () {
 
     const PARENT_PATH = "app/components/dialogs/controlInstanceDirective/";
 
+    // TODO Compute in parent component.
+    function areAllOptionsForced(model) {
+        for (let key in model) {
+            if (!model.hasOwnProperty(key)) {
+                continue;
+            }
+            if (!model[key].forced) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     function directiveFunction() {
         return {
-            "restrict": "A",
+            "restrict": "E",
             "scope": {
-                "ngModel": "=",
+                "dialog": "=lpDialog",
+                "application": "=lpApplication"
             },
             "templateUrl": PARENT_PATH + "dialogControlInstanceDirective.html",
             "link": (scope) => {
-
-                scope.$watch("ngModel", (model) => {
+                scope.$watch("dialog", (model) => {
                     if (model === undefined) {
                         scope.model = {};
                         return;
                     }
                     //
                     scope.model = model;
+                    scope.allForced = areAllOptionsForced(model);
                 });
+
+                scope.changeToHierarchyTab = function () {
+                    scope.application.changeToHierarchyTab();
+                }
 
             }
         };
