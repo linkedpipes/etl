cf6007e56fedb0ec1df0722cbbdac5164aa50427
Petr Škoda <skodapetr@gmail.com>
2017-07-11 15:48:12 +0200

    Add runtime configuration to  t-filesBase64Decode
    #302

--- plugins/t-filesBase64Decode/src/main/java/com/linkedpipes/etl/plugin/transformer/filedecode/FileDecode.java
+++ plugins/t-filesBase64Decode/src/main/java/com/linkedpipes/etl/plugin/transformer/filedecode/FileDecode.java
@@ -2,6 +2,7 @@ package com.linkedpipes.etl.plugin.transformer.filedecode;
 
 import com.linkedpipes.etl.dataunit.core.files.FilesDataUnit;
 import com.linkedpipes.etl.dataunit.core.files.WritableFilesDataUnit;
+import com.linkedpipes.etl.dataunit.core.rdf.SingleGraphDataUnit;
 import com.linkedpipes.etl.executor.api.v1.LpException;
 import com.linkedpipes.etl.executor.api.v1.component.Component;
 import com.linkedpipes.etl.executor.api.v1.component.SequentialExecution;
@@ -20,6 +21,10 @@ import java.util.Base64;
 public class FileDecode implements Component, SequentialExecution {
 
     private static final Logger LOG = LoggerFactory.getLogger(FileDecode.class);
+
+    @Component.ContainsConfiguration
+    @Component.InputPort(iri = "Configuration")
+    public SingleGraphDataUnit configurationRdf;
 
     @Component.InputPort(iri = "InputFiles")
     public FilesDataUnit inputFiles;
