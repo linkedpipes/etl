1b364925ff8ffd2e3feb08803842c258d8ce7eb4
Petr Škoda <skodapetr@gmail.com>
2017-09-15 11:45:53 +0200

    Remove unused code

--- plugins/e-sparqlEndpointChunkedList/src/main/java/com/linkedpipes/plugin/extractor/sparql/endpointlist/SparqlEndpointChunkedListVocabulary.java
+++ plugins/e-sparqlEndpointChunkedList/src/main/java/com/linkedpipes/plugin/extractor/sparql/endpointlist/SparqlEndpointChunkedListVocabulary.java
@@ -19,20 +19,6 @@ final class SparqlEndpointChunkedListVocabulary {
 
     public static final String HAS_FILE_NAME = PREFIX + "fileName";
 
-    public static final String REPORT = PREFIX + "Report";
-
-    public static final String HAS_TASK = PREFIX + "task";
-
-    public static final String HAS_EXCEPTION = PREFIX + "exception";
-
-    public static final String EXCEPTION = PREFIX + "Exception";
-
-    public static final String HAS_EXCEPTION_MESSAGE =
-            PREFIX + "message";
-
-    public static final String HAS_EXCEPTION_CLASS =
-            PREFIX + "class";
-
     public static final String HAS_USED_THREADS = PREFIX + "threads";
 
     public static final String HAS_TIME_LIMIT = PREFIX + "timeLimit";
