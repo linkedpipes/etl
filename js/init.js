7edbcec13f73dabeec96da01b7a924c1d4fa3d2d
Jakub Klímek <klimek@ksi.mff.cuni.cz>
2017-12-01 12:20:55 +0100

    Fix news section

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+	// Initialize collapse button
+	//$(".button-collapse").sideNav();
+	// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    $('.collapsible').collapsible();
+	$('.scrollspy').scrollSpy();
+	$('.toc-wrapper').pushpin({ top: $('.toc-wrapper').offset() ? $('.toc-wrapper').offset().top : 0 });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
