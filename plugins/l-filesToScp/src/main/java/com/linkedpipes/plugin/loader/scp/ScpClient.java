778acf92a52db45c2f02264642e52bf36e5aad4e
Petr Škoda <skodapetr@gmail.com>
2018-07-30 07:27:55 +0200

    Update ScpClient
    Add wait for closed command connection.

--- plugins/l-filesToScp/src/main/java/com/linkedpipes/plugin/loader/scp/ScpClient.java
+++ plugins/l-filesToScp/src/main/java/com/linkedpipes/plugin/loader/scp/ScpClient.java
@@ -1,5 +1,6 @@
 package com.linkedpipes.plugin.loader.scp;
 
+import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
@@ -14,6 +15,8 @@ import java.util.Collection;
 /**
  * Used library: http://www.jcraft.com/jsch/examples/ SCP protocol:
  * https://blogs.oracle.com/janp/entry/how_the_scp_protocol_works
+ *
+ * This class is not thread save.
  */
 class ScpClient implements AutoCloseable {
 
@@ -21,7 +24,9 @@ class ScpClient implements AutoCloseable {
 
     private static final int DATA_AWAIT_SLEEP_MS = 1000;
 
-    public static final int DATA_AWAIT_MAX_ITERATIONS = 10;
+    private static final int DATA_AWAIT_MAX_ITERATIONS = 10;
+
+    private static final int COMMAND_WAIT_MS = 500;
 
     @FunctionalInterface
     interface ChanelConsumer {
@@ -32,9 +37,13 @@ class ScpClient implements AutoCloseable {
 
     private final JSch jsch = new JSch();
 
+    private final int timeOut;
+
     private Session session = null;
 
-    private int timeOut;
+    private OutputStream remoteOutput;
+
+    private InputStream remoteInput;
 
     public ScpClient(int timeOut) {
         this.timeOut = timeOut;
@@ -45,7 +54,6 @@ class ScpClient implements AutoCloseable {
             throws Exception {
         LOG.debug("connect ...");
         this.session = jsch.getSession(username, hostname, port);
-        this.session.setServerAliveInterval(this.timeOut);
         this.session.setPassword(password);
         this.session.setTimeout(this.timeOut);
         configureSession();
@@ -63,7 +71,8 @@ class ScpClient implements AutoCloseable {
     public void createDirectory(String directory) throws Exception {
         LOG.info("createDirectory ...");
         withChannelExec((channel) -> {
-            channel.setCommand(this.createDirectoryCommand(directory));
+            String command = this.createDirectoryCommand(directory);
+            channel.setCommand(command);
             this.executeChannel(channel);
         });
         LOG.info("createDirectory ... done");
@@ -86,47 +95,155 @@ class ScpClient implements AutoCloseable {
     }
 
     private void executeChannel(ChannelExec channel) throws Exception {
+        ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
+        channel.setErrStream(errorStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        channel.setExtOutputStream(errorStream);
+        //
         channel.connect();
+        LOG.debug("Waiting for command to complete ...");
+        this.waitTillCommandIsFinished(channel);
         LOG.debug("Reading response status ...");
         int status = channel.getExitStatus();
-        LOG.debug("Reading response status ... done ({})", status);
+        LOG.info("Status code: {}", status);
         switch (status) {
-            case -1: // Fail with no output
+            case -1: // Fail with no output )command not done yet?).
                 throw new LpException("Action failed (-1).");
             case 0: // Ok
                 return;
             case 1: // Failure
             case 2: // Critical failure
-                this.handleError(status, channel);
+                LOG.info("Stderr: {}", errorStream.toString());
+                LOG.info("Stdout: {}", outputStream.toString());
+                throw new LpException(
+                        "Action failed, see log for more information.");
             default:
                 throw new LpException("Unexpected status: {}", status);
         }
     }
 
-    private void handleError(int status, ChannelExec channel)
-            throws IOException, LpException {
-        // ExtInput also includes err stream.
-        InputStream inputStream = channel.getExtInputStream();
-        String response;
-        try {
-            response = readResponse(inputStream);
-        } catch (IOException ex) {
-            response = "";
-            LOG.warn("Can't read server response.", ex);
+    private void waitTillCommandIsFinished(Channel channel) {
+        while (!channel.isClosed()) {
+            try {
+                Thread.sleep(COMMAND_WAIT_MS);
+            } catch (Exception e) {
+                // No operation here.
+            }
         }
-        LOG.error("Action failed, status: '{}' output: {}", status, response);
-        throw new LpException("Action failed, see log for more information.");
     }
 
-    private String readResponse(InputStream stream) throws IOException {
+    public void clearDirectory(String directory) throws Exception {
+        String command = "[ $(ls -A " + directory + " ) ] && rm -r " +
+                directory + "/* || :";
+        LOG.info("clearDirectory ... : {}", command);
+        withChannelExec((channel) -> {
+            channel.setCommand(command);
+            executeChannel(channel);
+        });
+        LOG.info("clearDirectory ... done");
+    }
+
+    public void uploadDirectories(
+            String directory, Collection<File> directories) throws Exception {
+        LOG.info("uploadDirectories ...");
+        String command = "scp -r -t -d " + directory;
+        withChannelExec((channel) -> {
+            ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
+            channel.setErrStream(errorStream);
+            try (OutputStream remoteOutput = channel.getOutputStream();
+                 InputStream remoteInput = channel.getInputStream()) {
+                this.remoteOutput = remoteOutput;
+                this.remoteInput = remoteInput;
+                //
+                channel.setCommand(command);
+                channel.connect();
+                this.checkResponseStream();
+                for (File file : directories) {
+                    this.sendDirectory(file);
+                }
+            }
+        });
+        LOG.info("uploadDirectories ... done");
+    }
+
+    private void sendDirectory(File sourceDirectory)
+            throws IOException, LpException {
+        for (File file : sourceDirectory.listFiles()) {
+            if (file.isDirectory()) {
+                this.sendDirectory(file, file.getName());
+            }
+            if (file.isFile()) {
+                this.sendFile(file, file.getName());
+            }
+        }
+    }
+
+    private void sendDirectory(File sourceDirectory, String directoryName)
+            throws IOException, LpException {
+        LOG.debug("Sending directory: {} ... ", directoryName);
+        // Send command.
+        String command = "D0755 0 " + directoryName + "\n";
+        this.remoteOutput.write(command.getBytes());
+        this.remoteOutput.flush();
+        this.checkResponseStream();
+        // Scan for files.
+        sendDirectory(sourceDirectory);
+        this.remoteOutput.write("E\n".getBytes());
+        this.remoteOutput.flush();
+        this.checkResponseStream();
+        LOG.debug("Sending directory: {} ... done", directoryName);
+    }
+
+    private void sendFile(File sourceFile, String fileName)
+            throws IOException, LpException {
+        LOG.debug("Sending file: {} ... ", fileName);
+        if (fileName.indexOf('/') > 0) {
+            throw new IllegalArgumentException("File name '" + fileName + "'");
+        }
+        // Send command for new file.
+        final Long fileSize = sourceFile.length();
+        String command = "C0644 " + fileSize + " " + fileName + "\n";
+        this.remoteOutput.write(command.getBytes());
+        this.remoteOutput.flush();
+        this.checkResponseStream();
+        // Copy file.
+        try (InputStream sourceFileStream = new FileInputStream(sourceFile)) {
+            IOUtils.copy(sourceFileStream, this.remoteOutput);
+        }
+        this.remoteOutput.flush();
+        // Write '\0' as the end of file.
+        this.remoteOutput.write(0);
+        this.remoteOutput.flush();
+        // Check status.
+        this.checkResponseStream();
+        LOG.debug("Sending file: {} ... done", fileName);
+    }
+
+    private void checkResponseStream() throws IOException, LpException {
+        int response = this.remoteInput.read();
+        switch (response) {
+            case -1: // No response from server.
+                throw new LpException("No response from server!");
+            case 0: // Success.
+                break;
+            case 1:
+                throw new LpException("Error: {}", this.readResponse());
+            case 2:
+                throw new LpException("Fatal error: {}", this.readResponse());
+            default:
+                throw new LpException("Invalid response: {}", response);
+        }
+    }
+
+    private String readResponse() throws IOException {
         LOG.debug("readResponse ...");
-        if (stream == null) {
+        if (this.remoteInput == null) {
             return "";
         }
-        this.waitForData(stream);
+        this.waitForData(this.remoteInput);
         StringBuffer buffer = new StringBuffer();
         while (true) {
-            int value = stream.read();
+            int value = this.remoteInput.read();
             if (value == 0 || value == -1) {
                 break;
             }
@@ -151,113 +268,6 @@ class ScpClient implements AutoCloseable {
                         " ms");
             }
         } while (stream.available() == 0);
-    }
-
-
-    public void clearDirectory(String directory) throws Exception {
-        String command = "[ $(ls -A " + directory + " ) ] && rm -r " +
-                directory + "/* || :";
-        LOG.info("clearDirectory ... : {}", command);
-        withChannelExec((channel) -> {
-            channel.setCommand(command);
-            executeChannel(channel);
-        });
-        LOG.info("clearDirectory ... done");
-    }
-
-    public void uploadDirectories(
-            String directory, Collection<File> directories) throws Exception {
-        LOG.info("uploadDirectories ...");
-        String command = "scp -r -t -d " + directory;
-        withChannelExec((channel) -> {
-            try (OutputStream remoteOut = channel.getOutputStream();
-                 InputStream remoteIn = channel.getInputStream()) {
-                //
-                channel.setCommand(command);
-                channel.connect();
-                checkResponseStream(remoteIn);
-                for (File file : directories) {
-                    sendDirectory(remoteOut, remoteIn, file);
-                }
-            }
-        });
-        LOG.info("uploadDirectories ... done");
-    }
-
-    private void sendDirectory(
-            OutputStream remoteOut, InputStream remoteIn, File sourceDirectory)
-            throws IOException, LpException {
-        for (File file : sourceDirectory.listFiles()) {
-            if (file.isDirectory()) {
-                sendDirectory(remoteOut, remoteIn, file, file.getName());
-            }
-            if (file.isFile()) {
-                sendFile(remoteOut, remoteIn, file, file.getName());
-            }
-        }
-    }
-
-    private void sendDirectory(
-            OutputStream remoteOut, InputStream remoteIn,
-            File sourceDirectory, String directoryName)
-            throws IOException, LpException {
-        LOG.debug("Sending directory: {} ... ", directoryName);
-        // Send command.
-        String command = "D0755 0 " + directoryName + "\n";
-        remoteOut.write(command.getBytes());
-        remoteOut.flush();
-        checkResponseStream(remoteIn);
-        // Scan for files.
-        sendDirectory(remoteOut, remoteIn, sourceDirectory);
-        remoteOut.write("E\n".getBytes());
-        remoteOut.flush();
-        checkResponseStream(remoteIn);
-        LOG.debug("Sending directory: {} ... done", directoryName);
-    }
-
-    private void sendFile(
-            OutputStream remoteOut, InputStream remoteIn,
-            File sourceFile, String fileName)
-            throws IOException, LpException {
-        LOG.debug("Sending file: {} ... ", fileName);
-        if (fileName.indexOf('/') > 0) {
-            throw new IllegalArgumentException("File name '" + fileName + "'");
-        }
-        // Send command for new file.
-        final Long fileSize = sourceFile.length();
-        String command = "C0644 " + fileSize + " " + fileName + "\n";
-        remoteOut.write(command.getBytes());
-        remoteOut.flush();
-        checkResponseStream(remoteIn);
-        // Copy file.
-        try (FileInputStream sourceFileStream
-                     = new FileInputStream(sourceFile)) {
-            IOUtils.copy(sourceFileStream, remoteOut);
-        }
-        remoteOut.flush();
-        // Write '\0' as the end of file.
-        remoteOut.write(0);
-        remoteOut.flush();
-        // Check status.
-        checkResponseStream(remoteIn);
-        LOG.debug("Sending file: {} ... done", fileName);
-    }
-
-    private void checkResponseStream(InputStream stream)
-            throws IOException, LpException {
-        int response = stream.read();
-        switch (response) {
-            case -1: // No response from server.
-                throw new LpException("No response from server!");
-            case 0: // Success.
-                break;
-            case 1:
-                throw new LpException("Error: {}", readResponse(stream));
-            case 2:
-                throw new LpException("Fatal error: {}", readResponse(stream));
-            default:
-                throw new LpException("Invalid response: {}", response);
-        }
     }
 
     @Override
