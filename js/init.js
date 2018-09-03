da1b225edd2448c0cfd53e209c1e8b90177cab87
Jakub Klímek <klimek@ksi.mff.cuni.cz>
2018-09-03 10:19:48 +0200

    Add favicons, remove jQuery

--- js/init.js
+++ js/init.js
@@ -1,11 +1,9 @@
-(function($){
-  $(function(){
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems);
+  });
 
-	// Initialize collapse button
-	//$(".button-collapse").sideNav();
-	// Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    $('.collapsible').collapsible();
-	$('.scrollspy').scrollSpy();
-	$('.toc-wrapper').pushpin({ top: $('.toc-wrapper').offset() ? $('.toc-wrapper').offset().top : 0 });
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
