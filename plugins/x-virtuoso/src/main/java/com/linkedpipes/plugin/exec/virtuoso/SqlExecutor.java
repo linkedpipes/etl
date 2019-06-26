f6a6f7ec633add9a155f488e86ba7cda8d4a472c
Petr Škoda <skodapetr@gmail.com>
2019-06-26 14:58:15 +0200

    Update imports

--- plugins/x-virtuoso/src/main/java/com/linkedpipes/plugin/exec/virtuoso/SqlExecutor.java
+++ plugins/x-virtuoso/src/main/java/com/linkedpipes/plugin/exec/virtuoso/SqlExecutor.java
@@ -2,10 +2,12 @@ package com.linkedpipes.plugin.exec.virtuoso;
 
 import com.linkedpipes.etl.executor.api.v1.LpException;
 import com.linkedpipes.etl.executor.api.v1.service.ExceptionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 class SqlExecutor {
 
