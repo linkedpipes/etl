6194eb754b3017e818b8c9b9ec9c89de814a3eea
Petr Škoda <skodapetr@gmail.com>
2019-07-05 09:27:30 +0200

    Add download button to debug view
    #701

--- frontend/client-debug/execution/debug-files/list-item-file.vue
+++ frontend/client-debug/execution/debug-files/list-item-file.vue
@@ -14,16 +14,34 @@
         {{ value.size }} bytes
       </v-list-tile-sub-title>
     </v-list-tile-content>
+    <v-btn
+      :href="downloadUrl"
+      target="_blank"
+      download
+      @click.stop="() => {}"
+    >
+      <v-icon>cloud_download</v-icon>
+    </v-btn>
   </v-list-tile>
 </template>
 
 <script>
   import Vue from "vue";
+  import {getDownloadDebugUrl} from "./debug-files-service";
 
   export default Vue.extend({
     "name": "debug-files-list-item-file",
     "props": {
       "value": {"type": Object, "required": true}
+    },
+    "computed": {
+      "downloadUrl": function () {
+        let path = this.$route.query["path"] + "/" + this.value["name"];
+        return getDownloadDebugUrl(
+          this.$route.params["execution"],
+          path,
+          this.value["source"]);
+      }
     }
   });
 </script>
