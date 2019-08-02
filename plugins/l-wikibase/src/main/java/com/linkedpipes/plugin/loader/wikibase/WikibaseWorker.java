3bf2c3cfe4d6f87b8a7a5dc704b8438d30112b19
Petr Škoda <skodapetr@gmail.com>
2019-08-02 12:19:05 +0200

    Fix l-wikibase

--- plugins/l-wikibase/src/main/java/com/linkedpipes/plugin/loader/wikibase/WikibaseWorker.java
+++ plugins/l-wikibase/src/main/java/com/linkedpipes/plugin/loader/wikibase/WikibaseWorker.java
@@ -3,15 +3,10 @@ package com.linkedpipes.plugin.loader.wikibase;
 import com.linkedpipes.etl.dataunit.core.rdf.WritableSingleGraphDataUnit;
 import com.linkedpipes.etl.executor.api.v1.LpException;
 import com.linkedpipes.etl.executor.api.v1.component.task.TaskConsumer;
-import com.linkedpipes.etl.executor.api.v1.rdf.RdfException;
 import com.linkedpipes.etl.executor.api.v1.rdf.model.RdfSource;
-import com.linkedpipes.etl.executor.api.v1.rdf.model.RdfValue;
 import com.linkedpipes.etl.executor.api.v1.service.ExceptionFactory;
-import com.linkedpipes.etl.executor.api.v1.vocabulary.SKOS;
+import com.linkedpipes.plugin.loader.wikibase.model.DocumentsLoader;
 import com.linkedpipes.plugin.loader.wikibase.model.WikibaseDocument;
-import com.linkedpipes.plugin.loader.wikibase.model.WikibaseStatement;
-import com.linkedpipes.plugin.loader.wikibase.model.Wikidata;
-import org.eclipse.rdf4j.model.vocabulary.RDFS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wikidata.wdtk.wikibaseapi.ApiConnection;
@@ -74,7 +69,9 @@ class WikibaseWorker implements TaskConsumer<WikibaseTask> {
 
     @Override
     public void accept(WikibaseTask task) throws LpException {
-        WikibaseDocument document = loadDocument(task);
+        DocumentsLoader loader = new DocumentsLoader(
+                configuration.getSiteIri(), source);
+        WikibaseDocument document = loader.loadDocument(task.getIri());
         LOG.debug("Processing: {}", task.getIri());
         try {
             synchronizer.synchronize(document);
@@ -83,67 +80,6 @@ class WikibaseWorker implements TaskConsumer<WikibaseTask> {
                     "Error processing document: {}",
                     document.getIri(), ex);
         }
-    }
-
-    private WikibaseDocument loadDocument(WikibaseTask task)
-            throws RdfException {
-        String statementPropertyPrefix =
-                configuration.getSiteIri() + "prop/";
-        String iri = task.getIri();
-//        WikibaseDocument document = new WikibaseDocument(iri);
-//        source.statements(iri, (predicate, value) -> {
-//            if (RDF.TYPE.equals(predicate)) {
-//                document.getTypes().add(value.asString());
-//            } else if (isLabel(predicate)) {
-//                document.setLabel(value.asString(), value.getLanguage());
-//            } else if (predicate.startsWith(statementPropertyPrefix)) {
-//                if (value.getType() != null) {
-//                    // It is not IRI.
-//                    return;
-//                }
-//                WikibaseStatement statement =
-//                        loadStatement(source, value, predicate);
-//                if (statement != null) {
-//                    document.addStatement(statement);
-//                }
-//            }
-//        });
-        return null;
-    }
-
-    private boolean isLabel(String predicate) {
-        return SKOS.PREF_LABEL.equals(predicate) ||
-                RDFS.LABEL.toString().equals(predicate);
-    }
-
-    /**
-     * Return null if the given entity is not Wikidata statement.
-     */
-    private WikibaseStatement loadStatement(
-            RdfSource source, RdfValue statementIri, String statementPredicate)
-            throws RdfException {
-//        WikibaseStatement statement =
-//                new WikibaseStatement(
-//                        statementIri.asString(), statementPredicate);
-//        String valuePredicate = configuration.getSiteIri() +
-//                "prop/statement/" + statement.getPredicate();
-//        source.statements(statementIri.asString(), (predicate, value) -> {
-//            if (RDF.TYPE.equals(predicate)) {
-//                statement.getTypes().add(value.asString());
-//            } else if (valuePredicate.equals(predicate)) {
-//                statement.setValue(value.asString());
-//            }
-//        });
-//        if (isStatement(statement)) {
-//            return statement;
-//        } else {
-//            return null;
-//        }
-        return null;
-    }
-
-    private boolean isStatement(WikibaseStatement statement) {
-        return statement.getTypes().contains(Wikidata.STATEMENT);
     }
 
     public void onAfterExecution() throws LpException {
