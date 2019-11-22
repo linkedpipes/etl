15c32aefa432ea1058f6978f1d2decd814a4f483
Petr Škoda <skodapetr@gmail.com>
2019-11-22 15:26:40 +0100

    Add wikibase files

---
+++ plugins/l-wikibase/src/main/java/com/linkedpipes/plugin/loader/wikibase/WikibaseTask.java
@@ -0,0 +1,18 @@
+package com.linkedpipes.plugin.loader.wikibase;
+
+import com.linkedpipes.etl.executor.api.v1.component.task.Task;
+
+public class WikibaseTask implements Task {
+
+    private final String iri;
+
+    public WikibaseTask(String iri) {
+        this.iri = iri;
+    }
+
+    @Override
+    public String getIri() {
+        return iri;
+    }
+
+}
