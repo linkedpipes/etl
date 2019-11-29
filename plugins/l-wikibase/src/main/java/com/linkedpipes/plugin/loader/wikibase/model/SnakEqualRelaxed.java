bce31458115ac1273f5122a933000fbcfc19e926
Petr Škoda <skodapetr@gmail.com>
2019-11-29 09:36:30 +0100

    Update l-wikibase
    #788

--- plugins/l-wikibase/src/main/java/com/linkedpipes/plugin/loader/wikibase/model/SnakEqualRelaxed.java
+++ plugins/l-wikibase/src/main/java/com/linkedpipes/plugin/loader/wikibase/model/SnakEqualRelaxed.java
@@ -1,5 +1,6 @@
 package com.linkedpipes.plugin.loader.wikibase.model;
 
+import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.QuantityValue;
 import org.wikidata.wdtk.datamodel.interfaces.Value;
 
@@ -30,7 +31,7 @@ class SnakEqualRelaxed implements SnakEqual {
                 bigDecimalEqual(
                         left.getNumericValue(), right.getNumericValue()) &&
                 left.getUnit().equals(right.getUnit()) &&
-                left.getUnitItemId().equals(right.getUnitItemId());
+                sameUnitItemId(left, right);
     }
 
     private boolean bigDecimalEqual(BigDecimal left, BigDecimal right) {
@@ -40,6 +41,15 @@ class SnakEqualRelaxed implements SnakEqual {
             return false;
         }
         return left.compareTo(right) == 0;
+    }
+
+    private boolean sameUnitItemId(QuantityValue left, QuantityValue right) {
+        ItemIdValue leftValue = left.getUnitItemId();
+        ItemIdValue rightValue = right.getUnitItemId();
+        if (leftValue == null) {
+            return rightValue == null;
+        }
+        return leftValue.equals(rightValue);
     }
 
 }
