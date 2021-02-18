37346d237ff701c0b7db89deed26aeb255cb94f2
Petr Škoda <skodapetr@gmail.com>
2021-02-18 11:05:40 +0100

    Add t-jsonLdFormatTitanium
    #174

---
+++ plugins/t-jsonLdFormatTitanium/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -0,0 +1,43 @@
+define([], function () {
+    "use strict";
+
+    const DESC = {
+        "$namespace": "http://plugins.linkedpipes.com/ontology/t-jsonLdFormatTitanium#",
+        "$type": "Configuration",
+        "$options": {
+            "$predicate": "auto",
+            "$control": "auto"
+        },
+        "format": {
+            "$type": "iri",
+            "$label": "Operation"
+        },
+        "context": {
+            "$type": "str",
+            "$label": "Context"
+        },
+        "frame": {
+            "$type": "str",
+            "$label": "Frame"
+        }
+    };
+
+    function controller($scope, $service) {
+
+        if ($scope.dialog === undefined) {
+            $scope.dialog = {};
+        }
+
+        const dialogManager = $service.v1.manager(DESC, $scope.dialog);
+
+        $service.onStore = function () {
+            dialogManager.save();
+        };
+
+        dialogManager.load();
+
+    }
+
+    controller.$inject = ['$scope', '$service'];
+    return controller;
+});
