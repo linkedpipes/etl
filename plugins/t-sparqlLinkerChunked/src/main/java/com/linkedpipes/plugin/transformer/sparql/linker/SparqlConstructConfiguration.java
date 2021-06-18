44d283207689b7de16b40003a755f1e70b3f157f
Petr Škoda <skodapetr@gmail.com>
2021-06-18 12:04:11 +0200

    t-sparqlLinkerChunked use ChunkExecution
    #840

--- plugins/t-sparqlLinkerChunked/src/main/java/com/linkedpipes/plugin/transformer/sparql/linker/SparqlConstructConfiguration.java
+++ plugins/t-sparqlLinkerChunked/src/main/java/com/linkedpipes/plugin/transformer/sparql/linker/SparqlConstructConfiguration.java
@@ -11,6 +11,12 @@ public class SparqlConstructConfiguration {
     @RdfToPojo.Property(iri = SparqlConstructVocabulary.HAS_OUTPUT_MODE)
     private String outputMode;
 
+    @RdfToPojo.Property(iri = SparqlConstructVocabulary.HAS_NUMBER_OF_THREADS)
+    private int threadCount = 1;
+
+    @RdfToPojo.Property(iri = SparqlConstructVocabulary.HAS_SKIP_ON_FAILURE)
+    private boolean skipOnFailure = false;
+
     public SparqlConstructConfiguration() {
     }
 
@@ -28,6 +34,22 @@ public class SparqlConstructConfiguration {
 
     public void setOutputMode(String outputMode) {
         this.outputMode = outputMode;
+    }
+
+    public int getThreadCount() {
+        return threadCount;
+    }
+
+    public void setThreadCount(int threadCount) {
+        this.threadCount = threadCount;
+    }
+
+    public boolean isSkipOnFailure() {
+        return skipOnFailure;
+    }
+
+    public void setSkipOnFailure(boolean skipOnFailure) {
+        this.skipOnFailure = skipOnFailure;
     }
 
 }
