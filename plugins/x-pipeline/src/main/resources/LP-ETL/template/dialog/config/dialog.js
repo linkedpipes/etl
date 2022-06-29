71fcbb2fa76e108d701ae1319b7ef2d7abc6775b
Jakub Klímek <jakub@jakubklimek.com>
2022-06-29 07:47:41 +0200

    Enhance labels

--- plugins/x-pipeline/src/main/resources/LP-ETL/template/dialog/config/dialog.js
+++ plugins/x-pipeline/src/main/resources/LP-ETL/template/dialog/config/dialog.js
@@ -14,7 +14,7 @@ define([], function () {
         },
         "pipeline": {
             "$type": "iri",
-            "$label": "Pipeline ULR"
+            "$label": "Pipeline IRI"
         },
         "saveDebugData": {
             "$type": "bool",
